ed9d6883b1ddf538eda25dabdc41af1a64ae7867
vauxhall <zero.griffin@gmail.com>
2017-10-17 03:52:34 -0500

    Add brand assets

--- Source/Model/Menu/MMenuItemHome.swift
+++ Source/Model/Menu/MMenuItemHome.swift
@@ -3,6 +3,6 @@ import UIKit
 struct MMenuItemHome:MMenuItemProtocol
 {
     let order:MMenu.Order = MMenu.Order.home
-    let icon:UIImage = #imageLiteral(resourceName: "assetGenericMenuHome")
+    let icon:UIImage = UIImage()
     let controllerType:UIViewController.Type = CHome.self
 }
