a30606113cd219c84caa92fe16d7c5dbbe3ec8f2
vauxhall <zero.griffin@gmail.com>
2017-10-17 12:39:27 -0500

    Add change status functionality

--- Source/Model/Create/MCreate.swift
+++ Source/Model/Create/MCreate.swift
@@ -2,4 +2,14 @@ import Foundation
 
 final class MCreate:Model<ArchCreate>
 {
+    private(set) var status:MCreateStatusProtocol?
+    
+    //MARK: internal
+    
+    func changeStatus(
+        statusType:MCreateStatusProtocol.Type)
+    {
+        let status:MCreateStatusProtocol = statusType.init()
+        self.status = status
+    }
 }
--- Source/Model/Create/MCreateStatusProtocol.swift
+++ Source/Model/Create/MCreateStatusProtocol.swift
@@ -3,4 +3,6 @@ import Foundation
 protocol MCreateStatusProtocol
 {
     var viewType:View<ArchCreate>.Type { get }
+    
+    init()
 }
