a05bb5664db5896fb9d0207a2eb99093f40d294b
vauxhall <zero.griffin@gmail.com>
2017-10-18 04:23:12 -0500

    Listen to app becoming active

--- Source/Model/Create/MCreateLocationStrategyDenied.swift
+++ Source/Model/Create/MCreateLocationStrategyDenied.swift
@@ -2,9 +2,34 @@ import Foundation
 
 final class MCreateLocationStrategyDenied:MCreateLocationStrategyProtocol
 {
+    private weak var model:MCreate?
+    
+    init()
+    {
+        NotificationCenter.default.addObserver(
+            self,
+            selector:#selector(selectorBecameActive(sender:)),
+            name:NSNotification.Name.UIApplicationDidBecomeActive,
+            object:nil)
+    }
+    
+    deinit
+    {
+        NotificationCenter.default.removeObserver(self)
+    }
+    
     func nextStep(model:MCreate)
     {
+        self.model = model
         model.changeStatus(
             statusType:MCreateStatusDenied.self)
+    }
+    
+    //MARK: selectors
+    
+    @objc
+    private func selectorBecameActive(sender notification:Notification)
+    {
+        model?.checkAuthorization()
     }
 }
