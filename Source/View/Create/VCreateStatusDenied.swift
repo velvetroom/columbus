e4f7b49f7f5901c4115aef7a2edc7850cb83bf91
vauxhall <zero.griffin@gmail.com>
2017-10-18 03:28:35 -0500

    Create view status denied

---
+++ Source/View/Create/VCreateStatusDenied.swift
@@ -0,0 +1,63 @@
+import UIKit
+
+final class VCreateStatusDenied:View<ArchCreate>
+{
+    private let kImageTop:CGFloat = 210
+    private let kImageHeight:CGFloat = 50
+    private let kLabelHeight:CGFloat = 200
+    
+    required init(controller:CCreate)
+    {
+        super.init(controller:controller)
+        
+        factoryViews()
+    }
+    
+    required init?(coder:NSCoder)
+    {
+        return nil
+    }
+    
+    //MARK: private
+    
+    private func factoryViews()
+    {
+        let imageView:UIImageView = UIImageView()
+        imageView.isUserInteractionEnabled = false
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.contentMode = UIViewContentMode.center
+        imageView.clipsToBounds = true
+        imageView.image = #imageLiteral(resourceName: "assetGenericError")
+        
+        let label:UILabel = UILabel()
+        label.isUserInteractionEnabled = false
+        label.backgroundColor = UIColor.clear
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.numberOfLines = 0
+        label.textAlignment = NSTextAlignment.center
+        
+        addSubview(imageView)
+        addSubview(label)
+        
+        NSLayoutConstraint.topToTop(
+            view:imageView,
+            toView:self,
+            constant:kImageTop)
+        NSLayoutConstraint.height(
+            view:imageView,
+            constant:kImageHeight)
+        NSLayoutConstraint.equalsHorizontal(
+            view:imageView,
+            toView:self)
+        
+        NSLayoutConstraint.topToBottom(
+            view:label,
+            toView:imageView)
+        NSLayoutConstraint.height(
+            view:label,
+            constant:kLabelHeight)
+        NSLayoutConstraint.equalsHorizontal(
+            view:label,
+            toView:self)
+    }
+}
