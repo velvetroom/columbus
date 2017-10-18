ce8bb8589b96c811887e7d93c7f782f022c6b2e6
vauxhall <zero.griffin@gmail.com>
2017-10-18 11:41:44 -0500

    Create factory extension

--- Source/View/Create/VCreateStatusReadyBar.swift
+++ Source/View/Create/VCreateStatusReadyBar.swift
@@ -2,5 +2,14 @@ import UIKit
 
 final class VCreateStatusReadyBar:View<ArchCreate>
 {
+    required init(controller:CCreate)
+    {
+        super.init(controller:controller)
+        backgroundColor = UIColor.colourSuccess
+    }
     
+    required init?(coder:NSCoder)
+    {
+        return nil
+    }
 }
