105e3b312823750f78b32833079f11a99a4562e5
vauxhall <zero.griffin@gmail.com>
2017-10-18 00:25:17 -0500

    Factory location strategy for status

--- Source/Model/Create/MCreateLocationFactory.swift
+++ Source/Model/Create/MCreateLocationFactory.swift
@@ -1,8 +1,57 @@
 import Foundation
+import CoreLocation
 
 extension MCreate
 {
+    //MARK: private
+    
+    private class func locationStrategyMap() -> [
+        CLAuthorizationStatus:MCreateLocationStrategyProtocol.Type]
+    {
+        let map:[CLAuthorizationStatus:
+            MCreateLocationStrategyProtocol.Type] = [
+                CLAuthorizationStatus.authorizedAlways:
+                    MCreateLocationStrategyGranted.self,
+                CLAuthorizationStatus.authorizedWhenInUse:
+                    MCreateLocationStrategyGranted.self,
+                CLAuthorizationStatus.notDetermined:
+                    MCreateLocationStrategyUnknown.self,
+                CLAuthorizationStatus.restricted:
+                    MCreateLocationStrategyDenied.self,
+                CLAuthorizationStatus.denied:
+                    MCreateLocationStrategyDenied.self]
+        
+        return map
+    }
+    
+    private class func locationStrategyType(
+        status:CLAuthorizationStatus) -> MCreateLocationStrategyProtocol.Type
+    {
+        let map:[
+            CLAuthorizationStatus:
+            MCreateLocationStrategyProtocol.Type] = locationStrategyMap()
+        
+        guard
+        
+            let type:MCreateLocationStrategyProtocol.Type = map[status]
+        
+        else
+        {
+            return MCreateLocationStrategyUnknown.self
+        }
+        
+        return type
+    }
+    
     //MARK: internal
     
-    
+    class func factoryLocationStrategy(
+        status:CLAuthorizationStatus) -> MCreateLocationStrategyProtocol
+    {
+        let strategyType:MCreateLocationStrategyProtocol.Type = locationStrategyType(
+            status:status)
+        let strategy:MCreateLocationStrategyProtocol = strategyType.init()
+        
+        return strategy
+    }
 }
