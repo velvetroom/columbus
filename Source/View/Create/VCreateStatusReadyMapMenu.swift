b685c43e9e1ec323ba3fe5552a2338ea1e5e18b8
vauxhall <zero.griffin@gmail.com>
2017-10-18 13:56:26 -0500

    Add collection item size

--- Source/View/Create/VCreateStatusReadyMapMenu.swift
+++ Source/View/Create/VCreateStatusReadyMapMenu.swift
@@ -4,5 +4,40 @@ final class VCreateStatusReadyMapMenu:VCollection<
     ArchCreate,
     VCreateStatusReadyMapMenuCell>
 {
+    private var cellSize:CGSize?
     
+    required init(controller:CCreate)
+    {
+        super.init(controller:controller)
+        collectionView.isScrollEnabled = false
+        collectionView.bounces = false
+    }
+    
+    required init?(coder:NSCoder)
+    {
+        return nil
+    }
+    
+    override func collectionView(
+        _ collectionView:UICollectionView,
+        layout collectionViewLayout:UICollectionViewLayout,
+        sizeForItemAt indexPath:IndexPath) -> CGSize
+    {
+        guard
+        
+            let cellSize:CGSize = self.cellSize
+        
+        else
+        {
+            let width:CGFloat = collectionView.bounds.width
+            let cellSize:CGSize = CGSize(
+                width:width,
+                height:width)
+            self.cellSize = cellSize
+            
+            return cellSize
+        }
+        
+        return cellSize
+    }
 }
