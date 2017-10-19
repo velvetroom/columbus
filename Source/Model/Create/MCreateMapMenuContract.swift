4dbbc291614613f8e3fa9f42b76b9c36026499ef
vauxhall <zero.griffin@gmail.com>
2017-10-19 05:07:29 -0500

    Update menu items
    -Change from class to struct
    -Add status type property
    -Remove selected function

--- Source/Model/Create/MCreateMapMenuContract.swift
+++ Source/Model/Create/MCreateMapMenuContract.swift
@@ -1,10 +1,7 @@
 import UIKit
 
-final class MCreateMapMenuContract:MCreateMapMenuProtocol
+struct MCreateMapMenuContract:MCreateMapMenuProtocol
 {
+    let statusType:MCreateMapStatusProtocol.Type = MCreateMapStatusContracted.self
     let icon:UIImage = #imageLiteral(resourceName: "assetMapButton")
-    
-    func selected(controller:CCreate)
-    {
-    }
 }
--- Source/Model/Create/MCreateMapMenuExpand.swift
+++ Source/Model/Create/MCreateMapMenuExpand.swift
@@ -1,10 +1,7 @@
 import UIKit
 
-final class MCreateMapMenuExpand:MCreateMapMenuProtocol
+struct MCreateMapMenuExpand:MCreateMapMenuProtocol
 {
+    let statusType:MCreateMapStatusProtocol.Type = MCreateMapStatusExpanded.self
     let icon:UIImage = #imageLiteral(resourceName: "assetMapButton")
-    
-    func selected(controller:CCreate)
-    {
-    }
 }
