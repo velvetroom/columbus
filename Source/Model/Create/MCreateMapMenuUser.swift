80dc129cd7ba4329eec5e5e2321e38d177e78d78
vauxhall <zero.griffin@gmail.com>
2017-10-19 10:39:08 -0500

    Create menu item user

---
+++ Source/Model/Create/MCreateMapMenuUser.swift
@@ -0,0 +1,7 @@
+import UIKit
+
+final class MCreateMapMenuUser:MCreateMapMenuProtocol
+{
+    let statusType:MCreateMapStatusProtocol.Type? = nil
+    let icon:UIImage = #imageLiteral(resourceName: "assetMapButtonUser")
+}
