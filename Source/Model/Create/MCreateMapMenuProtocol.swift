edb4cde11a9da80ce1ce0a74f590fa922d9c9a31
vauxhall <zero.griffin@gmail.com>
2017-10-19 05:04:47 -0500

    Update menu protocol
    -Add status type property
    -Remove selected function

--- Source/Model/Create/MCreateMapMenuProtocol.swift
+++ Source/Model/Create/MCreateMapMenuProtocol.swift
@@ -2,7 +2,6 @@ import UIKit
 
 protocol MCreateMapMenuProtocol
 {
+    var statusType:MCreateMapStatusProtocol.Type { get }
     var icon:UIImage { get }
-    
-    func selected(controller:CCreate)
 }
