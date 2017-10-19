eaf7a218ecfc2b6602ff46df47abd523c5c0e85d
vauxhall <zero.griffin@gmail.com>
2017-10-19 13:01:42 -0500

    Create item stop

---
+++ Source/Model/Create/MCreateMapMenuStop.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+final class MCreateMapMenuStop:MCreateMapMenuProtocol
+{
+    let statusType:MCreateMapStatusProtocol.Type? = nil
+    let icon:UIImage = #imageLiteral(resourceName: "assetMapButtonStop")
+    
+    func selected(controller:CCreate)
+    {
+    }
+}
+
