c7e0d8d78d221f332e0549339fc8b4bb65f8de21
vauxhall <zero.griffin@gmail.com>
2017-10-19 14:51:29 -0500

    Add computed var coordinate

--- Source/CoreData/Entities/DPlanStop.swift
+++ Source/CoreData/Entities/DPlanStop.swift
@@ -1,5 +1,23 @@
 import Foundation
+import CoreLocation
 
 extension DPlanStop
 {
+    var coordinate:CLLocationCoordinate2D
+    {
+        get
+        {
+            let coordinate:CLLocationCoordinate2D = CLLocationCoordinate2D(
+                latitude:latitude,
+                longitude:longitude)
+            
+            return coordinate
+        }
+        
+        set(newValue)
+        {
+            latitude = newValue.latitude
+            longitude = newValue.longitude
+        }
+    }
 }
