44632ff016eb51edbc40ca4250d745ed5b82d1f9
vauxhall <zero.griffin@gmail.com>
2017-10-19 05:33:04 -0500

    Layout menu cell

--- Source/View/Create/VCreateStatusReadyMapMenuCell.swift
+++ Source/View/Create/VCreateStatusReadyMapMenuCell.swift
@@ -3,5 +3,70 @@ import UIKit
 final class VCreateStatusReadyMapMenuCell:
     UICollectionViewCell
 {
+    private weak var icon:UIImageView!
+    private let kAlphaSelected:CGFloat = 0.3
+    private let kAlphaNotSelected:CGFloat = 1
     
+    override init(frame:CGRect)
+    {
+        super.init(frame:frame)
+        clipsToBounds = true
+        backgroundColor = UIColor.clear
+        
+        let icon:UIImageView = UIImageView()
+        icon.isUserInteractionEnabled = false
+        icon.translatesAutoresizingMaskIntoConstraints = false
+        icon.contentMode = UIViewContentMode.center
+        icon.clipsToBounds = true
+        self.icon = icon
+        
+        addSubview(icon)
+        
+        NSLayoutConstraint.equals(
+            view:icon,
+            toView:self)
+    }
+    
+    required init?(coder:NSCoder)
+    {
+        return nil
+    }
+    
+    override var isSelected:Bool
+    {
+        didSet
+        {
+            hover()
+        }
+    }
+    
+    override var isHighlighted:Bool
+    {
+        didSet
+        {
+            hover()
+        }
+    }
+    
+    //MARK: private
+    
+    private func hover()
+    {
+        if isSelected || isHighlighted
+        {
+            icon.alpha = kAlphaSelected
+        }
+        else
+        {
+            icon.alpha = kAlphaNotSelected
+        }
+    }
+    
+    //MARK: internal
+    
+    func config(model:MCreateMapMenuProtocol)
+    {
+        icon.image = model.icon
+        hover()
+    }
 }
