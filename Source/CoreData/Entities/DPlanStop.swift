73bd802d0c42b25226b95682036983193a1afe47
vauxhall <zero.griffin@gmail.com>
2017-10-20 00:46:02 -0500

    Create annotation extension

--- Source/CoreData/Entities/DPlanStop.swift
+++ Source/CoreData/Entities/DPlanStop.swift
@@ -1,23 +1,5 @@
 import Foundation
-import CoreLocation
 
 extension DPlanStop
 {
-    var coordinate:CLLocationCoordinate2D
-    {
-        get
-        {
-            let coordinate:CLLocationCoordinate2D = CLLocationCoordinate2D(
-                latitude:latitude,
-                longitude:longitude)
-            
-            return coordinate
-        }
-        
-        set(newValue)
-        {
-            latitude = newValue.latitude
-            longitude = newValue.longitude
-        }
-    }
 }
