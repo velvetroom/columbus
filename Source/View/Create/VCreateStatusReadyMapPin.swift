242fdd5713976882d52acf169f3a685bf501d03a
vauxhall <zero.griffin@gmail.com>
2017-10-20 06:03:23 -0500

    Add image to annotation

--- Source/View/Create/VCreateStatusReadyMapPin.swift
+++ Source/View/Create/VCreateStatusReadyMapPin.swift
@@ -2,6 +2,8 @@ import MapKit
 
 final class VCreateStatusReadyMapPin:MKAnnotationView
 {
+    private let kImageOffsetY:CGFloat = -20
+    
     init(stop:DPlanStop)
     {
         let reuseIdentifier:String = VCreateStatusReadyMapPin.reusableIdentifier
@@ -9,10 +11,52 @@ final class VCreateStatusReadyMapPin:MKAnnotationView
         super.init(
             annotation:stop,
             reuseIdentifier:reuseIdentifier)
+        
+        centerOffset = CGPoint(
+            x:0,
+            y:kImageOffsetY)
     }
     
     required init?(coder:NSCoder)
     {
         return nil
+    }
+    
+    override var annotation:MKAnnotation?
+    {
+        didSet
+        {
+            hover()
+        }
+    }
+    
+    override var isSelected:Bool
+    {
+        didSet
+        {
+            hover()
+        }
+    }
+    
+    override var isHighlighted:Bool
+    {
+        didSet
+        {
+            hover()
+        }
+    }
+    
+    //MARK: private
+    
+    private func hover()
+    {
+        if isSelected || isHighlighted
+        {
+            image = #imageLiteral(resourceName: "assetMapAnnotationSelected")
+        }
+        else
+        {
+            image = #imageLiteral(resourceName: "assetMapAnnotation")
+        }
     }
 }
