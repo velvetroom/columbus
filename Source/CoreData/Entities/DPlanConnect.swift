286b7cd43fde4ff1976bb98f99ffaae77d96edd3
vauxhall <zero.griffin@gmail.com>
2017-10-20 06:56:44 -0500

    Connect stop

--- Source/CoreData/Entities/DPlanConnect.swift
+++ Source/CoreData/Entities/DPlanConnect.swift
@@ -2,5 +2,64 @@ import Foundation
 
 extension DPlan
 {
+    //MARK: private
+    
+    private func connect(
+        database:Database,
+        stop:DPlanStop,
+        lastStop:DPlanStop,
+        completion:@escaping(() -> ()))
+    {
+        database.create
+        { [weak self] (travel:DPlanTravel) in
+            
+            self?.connect(
+                stop:stop,
+                lastStop:lastStop,
+                travel:travel,
+                completion:completion)
+        }
+    }
+    
+    private func connect(
+        stop:DPlanStop,
+        lastStop:DPlanStop,
+        travel:DPlanTravel,
+        completion:@escaping(() -> ()))
+    {
+        travel.origin = lastStop
+        travel.destination = stop
+        
+        completion()
+    }
+    
     //MARK: internal
+    
+    func connect(
+        database:Database,
+        stop:DPlanStop,
+        completion:@escaping(() -> ()))
+    {
+        defer
+        {
+            stop.plan = self
+        }
+        
+        guard
+        
+            let lastStop:DPlanStop = stops?.lastObject as? DPlanStop
+        
+        else
+        {
+            completion()
+            
+            return
+        }
+        
+        connect(
+            database:database,
+            stop:stop,
+            lastStop:lastStop,
+            completion:completion)
+    }
 }
--- Source/Model/Create/MCreatePlanStop.swift
+++ Source/Model/Create/MCreatePlanStop.swift
@@ -3,10 +3,57 @@ import CoreLocation
 
 extension MCreatePlan
 {
+    //MARK: private
+    
+    private func addStop(
+        stop:DPlanStop,
+        coordinate:CLLocationCoordinate2D)
+    {
+        stop.coordinate = coordinate
+        
+        plan.connect(
+            database:database,
+            stop:stop)
+        { [weak self] in
+        
+            self?.stopAdded(stop:stop)
+        }
+    }
+    
+    private func stopAdded(stop:DPlanStop)
+    {
+        DispatchQueue.main.async
+        { [weak self] in
+            
+            self?.addAnnotation(stop:stop)
+        }
+    }
+    
+    private func addAnnotation(stop:DPlanStop)
+    {
+        guard
+        
+            let view:VCreateStatusReady = model?.view?.view as? VCreateStatusReady
+        
+        else
+        {
+            return
+        }
+        
+        view.viewMap.addAnnotation(stop)
+    }
+    
+    
     //MARK: internal
     
     func addStop(coordinate:CLLocationCoordinate2D)
     {
-        
+        database.create
+        { [weak self] (stop:DPlanStop) in
+            
+            self?.addStop(
+                stop:stop,
+                coordinate:coordinate)
+        }
     }
 }
