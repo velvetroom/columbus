88f8d2d9c3b72774596ea64600db1248cdc1c6c5
vauxhall <zero.griffin@gmail.com>
2017-10-20 06:28:25 -0500

    Add coordinate at centre

--- Source/View/Create/VCreateStatusReadyMapActions.swift
+++ Source/View/Create/VCreateStatusReadyMapActions.swift
@@ -27,4 +27,19 @@ extension VCreateStatusReadyMap
         
         setRegion(region, animated:true)
     }
+    
+    func coordinateAtCentre() -> CLLocationCoordinate2D
+    {
+        let midX:CGFloat = bounds.midX
+        let midY:CGFloat = bounds.midY
+        let point:CGPoint = CGPoint(
+            x:midX,
+            y:midY)
+        
+        let location:CLLocationCoordinate2D = convert(
+            point,
+            toCoordinateFrom:self)
+        
+        return location
+    }
 }
