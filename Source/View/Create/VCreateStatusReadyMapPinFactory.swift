62952d9d83bebbcd15c72096ecddf2e91acd30cb
vauxhall <zero.griffin@gmail.com>
2017-10-20 11:04:56 -0500

    Create drag state factory

---
+++ Source/View/Create/VCreateStatusReadyMapPinFactory.swift
@@ -0,0 +1,27 @@
+import MapKit
+
+extension VCreateStatusReadyMapPin
+{
+    private static let kDragStateMap:[
+    MKAnnotationViewDragState:MKAnnotationViewDragState] = [
+        MKAnnotationViewDragState.starting:
+            MKAnnotationViewDragState.dragging]
+    
+    //MARK: internal
+    
+    class func factoryDragState(
+        state:MKAnnotationViewDragState) -> MKAnnotationViewDragState
+    {
+        guard
+        
+            let newState:MKAnnotationViewDragState = kDragStateMap[
+                state]
+        
+        else
+        {
+            return MKAnnotationViewDragState.none
+        }
+        
+        return newState
+    }
+}
