b19d6942c72f443484afd1ab187ce3763cf4c799
vauxhall <zero.griffin@gmail.com>
2017-10-20 05:58:01 -0500

    Add annotation assets

--- Source/View/Create/VCreateStatusReadyMapPin.swift
+++ Source/View/Create/VCreateStatusReadyMapPin.swift
@@ -2,4 +2,17 @@ import MapKit
 
 final class VCreateStatusReadyMapPin:MKAnnotationView
 {
+    init(stop:DPlanStop)
+    {
+        let reuseIdentifier:String = VCreateStatusReadyMapPin.reusableIdentifier
+        
+        super.init(
+            annotation:stop,
+            reuseIdentifier:reuseIdentifier)
+    }
+    
+    required init?(coder:NSCoder)
+    {
+        return nil
+    }
 }
