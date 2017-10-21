64d696c4dd30487cd2d14854a5e172ac757c65b5
vauxhall <zero.griffin@gmail.com>
2017-10-21 06:20:22 -0500

    Animate load progress

--- Source/View/Create/VCreateStatusReadyBarLoader.swift
+++ Source/View/Create/VCreateStatusReadyBarLoader.swift
@@ -3,5 +3,77 @@ import UIKit
 final class VCreateStatusReadyBarLoader:
     View<ArchCreate>
 {
+    private weak var viewProgress:UIView!
+    private weak var layoutProgressWidth:NSLayoutConstraint!
+    private let kAnimationDuration:TimeInterval = 10
+    private let kFadeDuration:TimeInterval = 0.3
     
+    required init(controller:CCreate)
+    {
+        super.init(controller:controller)
+        isUserInteractionEnabled = false
+        
+        let viewProgress:UIView = UIView()
+        viewProgress.isUserInteractionEnabled = false
+        viewProgress.translatesAutoresizingMaskIntoConstraints = false
+        viewProgress.clipsToBounds = true
+        viewProgress.backgroundColor = UIColor(white:1, alpha:0.3)
+        viewProgress.alpha = 0
+        self.viewProgress = viewProgress
+        
+        addSubview(viewProgress)
+        
+        NSLayoutConstraint.equalsVertical(
+            view:viewProgress,
+            toView:self)
+        NSLayoutConstraint.leftToLeft(
+            view:viewProgress,
+            toView:self)
+        layoutProgressWidth = NSLayoutConstraint.width(
+            view:viewProgress,
+            toView:self)
+    }
+    
+    required init?(coder:NSCoder)
+    {
+        return nil
+    }
+    
+    //MARK: private
+    
+    private func animateProgress()
+    {
+        viewProgress.alpha = 1
+        layoutProgressWidth.constant = bounds.width
+        
+        UIView.animate(
+            withDuration:kAnimationDuration,
+            animations:
+        { [weak self] in
+            
+            self?.layoutIfNeeded()
+        })
+        { [weak self] (done:Bool) in
+            
+            self?.animateFade()
+        }
+    }
+    
+    private func animateFade()
+    {
+        UIView.animate(withDuration:kFadeDuration)
+        { [weak self] in
+            
+            self?.viewProgress.alpha = 0
+        }
+    }
+    
+    //MARK: internal
+    
+    func loadProgress()
+    {
+        layoutProgressWidth.constant = 0
+        layoutIfNeeded()
+        animateProgress()
+    }
 }
