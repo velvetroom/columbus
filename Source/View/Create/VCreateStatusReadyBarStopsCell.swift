5d5748eb838227a669dd21b213db4883ae64ec54
vauxhall <zero.griffin@gmail.com>
2017-10-20 19:11:44 -0500

    Factory cell views

--- Source/View/Create/VCreateStatusReadyBarStopsCell.swift
+++ Source/View/Create/VCreateStatusReadyBarStopsCell.swift
@@ -3,11 +3,17 @@ import UIKit
 final class VCreateStatusReadyBarStopsCell:
     UICollectionViewCell
 {
+    let kFieldLeft:CGFloat = 100
+    let kRemoveWidth:CGFloat = 80
+    let kFieldMarginVertical:CGFloat = 5
+    
     override init(frame:CGRect)
     {
         super.init(frame:frame)
         clipsToBounds = true
         backgroundColor = UIColor.clear
+        
+        factoryViews()
     }
     
     required init?(coder:NSCoder)
--- Source/View/Create/VCreateStatusReadyBarStopsCellFactory.swift
+++ Source/View/Create/VCreateStatusReadyBarStopsCellFactory.swift
@@ -3,4 +3,31 @@ import UIKit
 extension VCreateStatusReadyBarStopsCell
 {
     //MARK: internal
+    
+    func factoryViews()
+    {
+        let buttonRemove:UIButton = UIButton()
+        buttonRemove.translatesAutoresizingMaskIntoConstraints = false
+        buttonRemove.setImage(
+            #imageLiteral(resourceName: "assetGenericDelete").withRenderingMode(UIImageRenderingMode.alwaysOriginal),
+            for:UIControlState.normal)
+        buttonRemove.setImage(
+            #imageLiteral(resourceName: "assetGenericDelete").withRenderingMode(UIImageRenderingMode.alwaysTemplate),
+            for:UIControlState.highlighted)
+        buttonRemove.imageView!.clipsToBounds = true
+        buttonRemove.imageView!.contentMode = UIViewContentMode.center
+        buttonRemove.imageView!.tintColor = UIColor.black
+        
+        addSubview(buttonRemove)
+        
+        NSLayoutConstraint.equalsVertical(
+            view:buttonRemove,
+            toView:self)
+        NSLayoutConstraint.rightToRight(
+            view:buttonRemove,
+            toView:self)
+        NSLayoutConstraint.width(
+            view:buttonRemove,
+            constant:kRemoveWidth)
+    }
 }
