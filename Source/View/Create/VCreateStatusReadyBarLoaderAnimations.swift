130bba824e73cbf08a3e7194f6a37462150b6afe
vauxhall <zero.griffin@gmail.com>
2017-10-21 07:43:01 -0500

    Move animations to extension

--- Source/View/Create/VCreateStatusReadyBarLoaderAnimations.swift
+++ Source/View/Create/VCreateStatusReadyBarLoaderAnimations.swift
@@ -2,5 +2,69 @@ import UIKit
 
 extension VCreateStatusReadyBarLoader
 {
+    //MARK: private
     
+    private func semaphoreSignaled()
+    {
+        DispatchQueue.main.async
+        { [weak self] in
+            
+            self?.asyncLoadProgress()
+        }
+    }
+    
+    private func asyncLoadProgress()
+    {
+        layer.removeAllAnimations()
+        viewProgress.layer.removeAllAnimations()
+        
+        layoutProgressWidth.constant = 0
+        layoutIfNeeded()
+        animateProgress()
+    }
+    
+    private func animateProgress()
+    {
+        viewProgress.alpha = 1
+        layoutProgressWidth.constant = bounds.width
+        
+        UIView.animate(
+            withDuration:kAnimationDuration,
+            animations:
+        { [weak self] in
+            
+            self?.layoutIfNeeded()
+        })
+        { [weak self] (done:Bool) in
+            
+            self?.animateFade()
+        }
+    }
+    
+    private func animateFade()
+    {
+        UIView.animate(
+            withDuration:kFadeDuration,
+            animations:
+        { [weak self] in
+            
+            self?.viewProgress.alpha = 0
+        })
+        { [weak self] (done:Bool) in
+            
+            self?.dispatchSemaphore.signal()
+        }
+    }
+    
+    //MARK: internal
+    
+    func loadProgress()
+    {
+        dispatchQueue.async
+        { [weak self] in
+            
+            self?.dispatchSemaphore.wait()
+            self?.semaphoreSignaled()
+        }
+    }
 }
