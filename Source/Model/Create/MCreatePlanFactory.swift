e3d9849c17d62a37da0184df9f2d0aa0a67267a9
vauxhall <zero.griffin@gmail.com>
2017-10-22 14:07:54 -0500

    Create factory extension

--- Source/Model/Create/MCreatePlanFactory.swift
+++ Source/Model/Create/MCreatePlanFactory.swift
@@ -4,4 +4,23 @@ import CoreLocation
 extension MCreatePlan
 {
     //MARK: internal
+    
+    class func factoryLocation(
+        coordinate:CLLocationCoordinate2D) -> CLLocation
+    {
+        let location:CLLocation = CLLocation(
+            latitude:coordinate.latitude,
+            longitude:coordinate.longitude)
+        
+        return location
+    }
+    
+    class func factoryLocation(
+        stop:DPlanStop) -> CLLocation
+    {
+        let location:CLLocation = factoryLocation(
+            coordinate:stop.coordinate)
+        
+        return location
+    }
 }
