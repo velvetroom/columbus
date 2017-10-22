126a23644d6d67c873dbcce4c83c4061561d49a9
vauxhall <zero.griffin@gmail.com>
2017-10-22 14:54:06 -0500

    Create extension for add stop

--- Source/Model/Create/MCreatePlanStop.swift
+++ Source/Model/Create/MCreatePlanStop.swift
@@ -5,82 +5,6 @@ extension MCreatePlan
 {
     //MARK: private
     
-    private func addStop(
-        name:String,
-        coordinate:CLLocationCoordinate2D)
-    {
-        database.create
-        { [weak self] (stop:DPlanStop) in
-            
-            self?.addStop(
-                stop:stop,
-                name:name,
-                coordinate:coordinate)
-        }
-    }
-    
-    private func addStop(
-        stop:DPlanStop,
-        name:String,
-        coordinate:CLLocationCoordinate2D)
-    {
-        stop.name = name
-        stop.coordinate = coordinate
-        
-        plan.connect(
-            database:database,
-            stop:stop)
-        { [weak self] in
-        
-            self?.stopAdded(stop:stop)
-        }
-    }
-    
-    private func stopAdded(stop:DPlanStop)
-    {
-        DispatchQueue.main.async
-        { [weak self] in
-            
-            self?.addAnnotation(stop:stop)
-            self?.selectLastStop()
-        }
-    }
-    
-    private func addAnnotation(stop:DPlanStop)
-    {
-        guard
-        
-            let view:VCreateStatusReady = model?.view?.view as? VCreateStatusReady
-        
-        else
-        {
-            return
-        }
-        
-        view.viewMap.addAnnotation(stop)
-        view.viewMap.centreAndSelect(
-            stop:stop)
-    }
-    
-    private func selectLastStop()
-    {
-        guard
-            
-            let view:VCreateStatusReady = model?.view?.view as? VCreateStatusReady,
-            let totalStops:Int = plan.stops?.count
-            
-        else
-        {
-            return
-        }
-        
-        let lastStop:Int = totalStops - 1
-        
-        view.viewBar.reload()
-        view.viewBar.viewStops.selectItem(
-            index:lastStop)
-    }
-    
     private func updated(stop:DPlanStop)
     {
         DispatchQueue.main.async
@@ -107,27 +31,35 @@ extension MCreatePlan
     
     //MARK: internal
     
-    func addStop(coordinate:CLLocationCoordinate2D)
+    func updateStops()
     {
-        let location:CLLocation = MCreatePlan.factoryLocation(
-            coordinate:coordinate)
-        
-        geocodeLocation(location:location)
-        { [weak self] (name:String?) in
+        guard
             
-            guard
-                
-                let name:String = name
+            let view:VCreateStatusReady = model?.view?.view as? VCreateStatusReady
             
-            else
-            {
-                return
-            }
-            
-            self?.addStop(
-                name:name,
-                coordinate:coordinate)
+        else
+        {
+            return
         }
+        
+        view.viewBar.reload()
+    }
+    
+    func selectLastStop()
+    {
+        guard
+            
+            let view:VCreateStatusReady = model?.view?.view as? VCreateStatusReady,
+            let totalStops:Int = plan.stops?.count
+            
+        else
+        {
+            return
+        }
+        
+        let lastStop:Int = totalStops - 1
+        view.viewBar.viewStops.selectItem(
+            index:lastStop)
     }
     
     func update(stop:DPlanStop)
---
+++ Source/Model/Create/MCreatePlanStopAdd.swift
@@ -0,0 +1,95 @@
+import Foundation
+import CoreLocation
+
+extension MCreatePlan
+{
+    //MARK: private
+    
+    private func addStop(
+        name:String,
+        coordinate:CLLocationCoordinate2D)
+    {
+        database.create
+        { [weak self] (stop:DPlanStop) in
+            
+            self?.addStop(
+                stop:stop,
+                name:name,
+                coordinate:coordinate)
+        }
+    }
+    
+    private func addStop(
+        stop:DPlanStop,
+        name:String,
+        coordinate:CLLocationCoordinate2D)
+    {
+        stop.name = name
+        stop.coordinate = coordinate
+        
+        plan.connect(
+            database:database,
+            stop:stop)
+        { [weak self] in
+            
+            self?.stopAdded(stop:stop)
+        }
+    }
+    
+    private func stopAdded(stop:DPlanStop)
+    {
+        DispatchQueue.main.async
+        { [weak self] in
+            
+            self?.asyncStopAdded(stop:stop)
+        }
+    }
+    
+    private func asyncStopAdded(stop:DPlanStop)
+    {
+        addAnnotation(stop:stop)
+        updateStops()
+        selectLastStop()
+    }
+    
+    private func addAnnotation(stop:DPlanStop)
+    {
+        guard
+            
+            let view:VCreateStatusReady = model?.view?.view as? VCreateStatusReady
+            
+        else
+        {
+            return
+        }
+        
+        view.viewMap.addAnnotation(stop)
+        view.viewMap.centreAndSelect(
+            stop:stop)
+    }
+    
+    //MARK: internal
+    
+    func addStop(coordinate:CLLocationCoordinate2D)
+    {
+        let location:CLLocation = MCreatePlan.factoryLocation(
+            coordinate:coordinate)
+        
+        geocodeLocation(location:location)
+        { [weak self] (name:String?) in
+            
+            guard
+                
+                let name:String = name
+                
+                else
+            {
+                return
+            }
+            
+            self?.addStop(
+                name:name,
+                coordinate:coordinate)
+        }
+    }
+}
