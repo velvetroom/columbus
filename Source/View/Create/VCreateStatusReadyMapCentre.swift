d7964bb7f0622e91af0b63e5e91a3b15be1853d0
vauxhall <zero.griffin@gmail.com>
2017-10-22 12:13:01 -0500

    Move centring methods to extension

--- Source/View/Create/VCreateStatusReadyMapCentre.swift
+++ Source/View/Create/VCreateStatusReadyMapCentre.swift
@@ -4,5 +4,53 @@ extension VCreateStatusReadyMap
 {
     //MARK: internal
     
+    func centreAndSelect(stop:DPlanStop)
+    {
+        setCenter(
+            stop.coordinate,
+            animated:true)
+        selectAnnotation(
+            stop,
+            animated:true)
+    }
     
+    func centreUser()
+    {
+        guard
+            
+            let coordinate:CLLocationCoordinate2D = userLocation.location?.coordinate
+            
+        else
+        {
+            return
+        }
+        
+        centreCoordinateRegion(coordinate:coordinate)
+        selectedAnnotations = []
+    }
+    
+    func centreCoordinateRegion(
+        coordinate:CLLocationCoordinate2D)
+    {
+        let region:MKCoordinateRegion = MKCoordinateRegionMake(
+            coordinate,
+            span)
+        
+        setRegion(region, animated:true)
+    }
+    
+    func coordinateAtCentre() -> CLLocationCoordinate2D
+    {
+        let midX:CGFloat = bounds.midX
+        let midY:CGFloat = bounds.midY
+        let point:CGPoint = CGPoint(
+            x:midX,
+            y:midY)
+        
+        let location:CLLocationCoordinate2D = convert(
+            point,
+            toCoordinateFrom:self)
+        
+        return location
+    }
 }
