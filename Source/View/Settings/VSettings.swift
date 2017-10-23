500f10ba2b52adc48c73d5036820db8a41acdadc
vauxhall <zero.griffin@gmail.com>
2017-10-23 17:01:17 -0500

    Layout settings bar

--- Source/View/Settings/VSettings.swift
+++ Source/View/Settings/VSettings.swift
@@ -2,4 +2,26 @@ import UIKit
 
 final class VSettings:ViewMain
 {
+    let kBarHeight:CGFloat = 64
+    
+    required init(controller:UIViewController)
+    {
+        super.init(controller:controller)
+        
+        guard
+        
+            let controller:CSettings = controller as? CSettings
+        
+        else
+        {
+            return
+        }
+        
+        factoryViews(controller:controller)
+    }
+    
+    required init?(coder:NSCoder)
+    {
+        return nil
+    }
 }
--- Source/View/Settings/VSettingsBar.swift
+++ Source/View/Settings/VSettingsBar.swift
@@ -2,5 +2,55 @@ import UIKit
 
 final class VSettingsBar:View<ArchSettings>
 {
+    private let kTitleTop:CGFloat = 20
+    private let kBorderHeight:CGFloat = 1
+    private let kFontSize:CGFloat = 15
     
+    required init(controller:CSettings)
+    {
+        super.init(controller:controller)
+        backgroundColor = UIColor.white
+        
+        let border:VBorder = VBorder(
+            colour:UIColor(white:0, alpha:0.2))
+        
+        let labelTitle:UILabel = UILabel()
+        labelTitle.isUserInteractionEnabled = false
+        labelTitle.translatesAutoresizingMaskIntoConstraints = false
+        labelTitle.backgroundColor = UIColor.clear
+        labelTitle.textAlignment = NSTextAlignment.center
+        labelTitle.font = UIFont.medium(size:kFontSize)
+        labelTitle.textColor = UIColor.colourBackgroundDark
+        labelTitle.text = String.localizedView(
+            key:"VSettingsBar_labelTitle")
+        
+        addSubview(border)
+        addSubview(labelTitle)
+        
+        NSLayoutConstraint.bottomToBottom(
+            view:border,
+            toView:self)
+        NSLayoutConstraint.height(
+            view:border,
+            constant:kBorderHeight)
+        NSLayoutConstraint.equalsHorizontal(
+            view:border,
+            toView:self)
+        
+        NSLayoutConstraint.topToTop(
+            view:labelTitle,
+            toView:self,
+            constant:kTitleTop)
+        NSLayoutConstraint.bottomToBottom(
+            view:labelTitle,
+            toView:self)
+        NSLayoutConstraint.equalsHorizontal(
+            view:labelTitle,
+            toView:self)
+    }
+    
+    required init?(coder:NSCoder)
+    {
+        return nil
+    }
 }
