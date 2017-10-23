777847db6f0319ccd691b9261a4cbeddb8054602
vauxhall <zero.griffin@gmail.com>
2017-10-23 09:04:34 -0500

    Remove annotation after disconnect

--- Source/Model/Create/MCreatePlanStopRemove.swift
+++ Source/Model/Create/MCreatePlanStopRemove.swift
@@ -9,8 +9,32 @@ extension MCreatePlan
             stop:stop)
         { [weak self] in
             
-            
+            self?.removedDone(stop:stop)
         }
+    }
+    
+    private func removedDone(stop:DPlanStop)
+    {
+        DispatchQueue.main.async
+        { [weak self] in
+            
+            self?.removeAnnotation(stop:stop)
+            self?.updateStops()
+        }
+    }
+    
+    private func removeAnnotation(stop:DPlanStop)
+    {
+        guard
+            
+            let view:VCreateStatusReady = model?.view?.view as? VCreateStatusReady
+            
+        else
+        {
+            return
+        }
+        
+        view.viewMap.removeAnnotation(stop)
     }
     
     //MARK: internal
