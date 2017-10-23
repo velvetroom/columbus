18d5e3a58462ca4826756f8c0eaa01bab99e9758
vauxhall <zero.griffin@gmail.com>
2017-10-23 17:29:23 -0500

    Add computed travel mode

--- Source/CoreData/Entities/DSettings.swift
+++ Source/CoreData/Entities/DSettings.swift
@@ -2,5 +2,26 @@ import Foundation
 
 extension DSettings
 {
-    
+    var travelMode:DPlanTravelMode
+    {
+        get
+        {
+            guard
+                
+                let travelMode:DPlanTravelMode = DPlanTravelMode(
+                    rawValue:rawTravelMode)
+                
+            else
+            {
+                return DPlanTravelMode.unknown
+            }
+            
+            return travelMode
+        }
+        
+        set(newValue)
+        {
+            rawTravelMode = newValue.rawValue
+        }
+    }
 }
