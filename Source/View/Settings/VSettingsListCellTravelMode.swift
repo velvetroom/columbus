97e5fc94bf9bad004099fb7cfcb45a849654e3d4
vauxhall <zero.griffin@gmail.com>
2017-10-24 08:34:11 -0500

    Factory view list

--- Source/View/Settings/VSettingsListCellTravelMode.swift
+++ Source/View/Settings/VSettingsListCellTravelMode.swift
@@ -3,6 +3,8 @@ import UIKit
 final class VSettingsListCellTravelMode:VSettingsListCell
 {
     private weak var model:MSettingsTravelMode?
+    private weak var viewList:VSettingsListCellTravelModeList?
+    private let kTitleHeight:CGFloat = 50
     
     override init(frame:CGRect)
     {
@@ -32,5 +34,41 @@ final class VSettingsListCellTravelMode:VSettingsListCell
         }
         
         self.model = model
+        
+        factoryList()
+    }
+    
+    //MARK: private
+    
+    private func factoryList()
+    {
+        guard
+        
+            self.viewList == nil,
+            let controller:CSettings = self.controller
+        
+        else
+        {
+            self.viewList?.collectionView.reloadData()
+            
+            return
+        }
+        
+        let viewList:VSettingsListCellTravelModeList = VSettingsListCellTravelModeList(
+            controller:controller)
+        self.viewList = viewList
+        
+        addSubview(viewList)
+        
+        NSLayoutConstraint.topToTop(
+            view:viewList,
+            toView:self,
+            constant:kTitleHeight)
+        NSLayoutConstraint.bottomToBottom(
+            view:viewList,
+            toView:self)
+        NSLayoutConstraint.equalsHorizontal(
+            view:viewList,
+            toView:self)
     }
 }
