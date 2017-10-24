2be452bed86b11bbb9f0fa16e708542afd58fe53
vauxhall <zero.griffin@gmail.com>
2017-10-24 14:31:50 -0500

    Add items to factory

--- Source/Model/Settings/MSettingsTravelModeFactory.swift
+++ Source/Model/Settings/MSettingsTravelModeFactory.swift
@@ -7,8 +7,14 @@ extension MSettingsTravelMode
     class func factoryItems() -> [MSettingsTravelModeProtocol]
     {
         let itemDriving:MSettingsTravelModeDriving = MSettingsTravelModeDriving()
+        let itemCycling:MSettingsTravelModeCycling = MSettingsTravelModeCycling()
+        let itemTransit:MSettingsTravelModeTransit = MSettingsTravelModeTransit()
+        let itemWalking:MSettingsTravelModeWalking = MSettingsTravelModeWalking()
         
         let items:[MSettingsTravelModeProtocol] = [
+            itemWalking,
+            itemCycling,
+            itemTransit,
             itemDriving]
         
         return items
