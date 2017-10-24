21d9bf141f384f6efde02cb7d1808fa2e683328a
vauxhall <zero.griffin@gmail.com>
2017-10-24 08:00:20 -0500

    Add collection delegate/data source

--- Source/View/Settings/VSettingsList.swift
+++ Source/View/Settings/VSettingsList.swift
@@ -30,4 +30,61 @@ final class VSettingsList:VCollection<
     {
         return nil
     }
+    
+    override func collectionView(
+        _ collectionView:UICollectionView,
+        layout collectionViewLayout:UICollectionViewLayout,
+        sizeForItemAt indexPath:IndexPath) -> CGSize
+    {
+        let item:MSettingsProtocol = modelAtIndex(
+            index:indexPath)
+        let width:CGFloat = collectionView.bounds.width
+        let size:CGSize = CGSize(
+            width:width,
+            height:item.cellHeight)
+        
+        return size
+    }
+    
+    override func collectionView(
+        _ collectionView:UICollectionView,
+        numberOfItemsInSection section:Int) -> Int
+    {
+        let count:Int = controller.model.items.count
+        
+        return count
+    }
+    
+    override func collectionView(
+        _ collectionView:UICollectionView,
+        cellForItemAt indexPath:IndexPath) -> UICollectionViewCell
+    {
+        let item:MSettingsProtocol = modelAtIndex(
+            index:indexPath)
+        let cell:VSettingsListCell = cellAtIndex(
+            reusableIdentifier:item.reusableIdentifier,
+            indexPath:indexPath)
+        
+        cell.config(model:item)
+        
+        return cell
+    }
+    
+    override func collectionView(
+        _ collectionView:UICollectionView,
+        shouldSelectItemAt indexPath:IndexPath) -> Bool
+    {
+        return false
+    }
+    
+    override func collectionView(
+        _ collectionView:UICollectionView,
+        shouldHighlightItemAt indexPath:IndexPath) -> Bool
+    {
+        return false
+    }
+    
+    override func collectionView(
+        _ collectionView:UICollectionView,
+        didSelectItemAt indexPath:IndexPath) { }
 }
--- Source/View/Settings/VSettingsListActions.swift
+++ Source/View/Settings/VSettingsListActions.swift
@@ -4,5 +4,11 @@ extension VSettingsList
 {
     //MARK: internal
     
-    
+    func modelAtIndex(index:IndexPath) -> MSettingsProtocol
+    {
+        let item:MSettingsProtocol = controller.model.items[
+            index.item]
+        
+        return item
+    }
 }
