5ce40a5fb11c237a391dd5afe1dcf484c5dceded
vauxhall <zero.griffin@gmail.com>
2017-10-24 14:27:59 -0500

    Create item cycling

---
+++ Source/Model/Settings/MSettingsTravelModeCycling.swift
@@ -0,0 +1,7 @@
+import UIKit
+
+struct MSettingsTravelModeCycling:MSettingsTravelModeProtocol
+{
+    let mode:DPlanTravelMode = DPlanTravelMode.cycling
+    let icon:UIImage = #imageLiteral(resourceName: "assetGenericCycling")
+}
