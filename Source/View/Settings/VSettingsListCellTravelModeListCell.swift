87b63c60900325c570038ff99bf27aa8114c1355
vauxhall <zero.griffin@gmail.com>
2017-10-24 08:51:15 -0500

    Layout cell

--- Source/View/Settings/VSettingsListCellTravelModeListCell.swift
+++ Source/View/Settings/VSettingsListCellTravelModeListCell.swift
@@ -3,11 +3,26 @@ import UIKit
 final class VSettingsListCellTravelModeListCell:
     UICollectionViewCell
 {
+    private weak var imageView:UIImageView!
+    
     override init(frame:CGRect)
     {
         super.init(frame:frame)
         clipsToBounds = true
         backgroundColor = UIColor.clear
+        
+        let imageView:UIImageView = UIImageView()
+        imageView.isUserInteractionEnabled = false
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.contentMode = UIViewContentMode.center
+        imageView.clipsToBounds = true
+        self.imageView = imageView
+        
+        addSubview(imageView)
+        
+        NSLayoutConstraint.equals(
+            view:imageView,
+            toView:imageView)
     }
     
     required init?(coder:NSCoder)
@@ -15,10 +30,42 @@ final class VSettingsListCellTravelModeListCell:
         return nil
     }
     
+    override var isSelected:Bool
+    {
+        didSet
+        {
+            hover()
+        }
+    }
+    
+    override var isHighlighted:Bool
+    {
+        didSet
+        {
+            hover()
+        }
+    }
+    
+    //MARK: private
+    
+    private func hover()
+    {
+        if isSelected || isHighlighted
+        {
+            imageView.tintColor = UIColor.white
+        }
+        else
+        {
+            imageView.tintColor = UIColor(white:0, alpha:0.3)
+        }
+    }
+    
     //MARK: internal
     
     func config(model:MSettingsTravelModeProtocol)
     {
-        
+        imageView.image = model.icon.withRenderingMode(
+            UIImageRenderingMode.alwaysTemplate)
+        hover()
     }
 }
