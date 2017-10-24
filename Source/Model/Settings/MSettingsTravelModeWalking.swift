0afc73036da3c828e62fbbb66811866f6c3f84a8
vauxhall <zero.griffin@gmail.com>
2017-10-24 14:29:18 -0500

    Create asset walking

---
+++ Source/Model/Settings/MSettingsTravelModeWalking.swift
@@ -0,0 +1,7 @@
+import UIKit
+
+struct MSettingsTravelModeWalking:MSettingsTravelModeProtocol
+{
+    let mode:DPlanTravelMode = DPlanTravelMode.walking
+    let icon:UIImage = #imageLiteral(resourceName: "assetGenericWalking")
+}
