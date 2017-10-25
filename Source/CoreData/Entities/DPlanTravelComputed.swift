a1fb31f5114b2555dff47e301fd104938d729500
vauxhall <zero.griffin@gmail.com>
2017-10-25 12:29:14 -0500

    Add computed properties extension

---
+++ Source/CoreData/Entities/DPlanTravelComputed.swift
@@ -0,0 +1,50 @@
+import Foundation
+
+extension DPlanTravel
+{
+    var status:DPlanTravelStatus
+    {
+        get
+        {
+            guard
+                
+                let status:DPlanTravelStatus = DPlanTravelStatus(
+                    rawValue:rawStatus)
+                
+            else
+            {
+                return DPlanTravelStatus.unknown
+            }
+            
+            return status
+        }
+        
+        set(newValue)
+        {
+            rawStatus = newValue.rawValue
+        }
+    }
+    
+    var mode:DPlanTravelMode
+    {
+        get
+        {
+            guard
+                
+                let mode:DPlanTravelMode = DPlanTravelMode(
+                    rawValue:rawMode)
+                
+            else
+            {
+                return DPlanTravelMode.unknown
+            }
+            
+            return mode
+        }
+        
+        set(newValue)
+        {
+            rawMode = newValue.rawValue
+        }
+    }
+}
