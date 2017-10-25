c72867dd535bd28d74a2cdc3bf140c69e91300b9
vauxhall <zero.griffin@gmail.com>
2017-10-25 09:51:03 -0500

    Factory map item

--- Source/Model/Create/MCreatePlanDirections.swift
+++ Source/Model/Create/MCreatePlanDirections.swift
@@ -2,6 +2,40 @@ import MapKit
 
 extension MCreatePlan
 {
+    //MARK: private
+    
+    private func factoryRequest(
+        travel:DPlanTravel) -> MKDirectionsRequest?
+    {
+        guard
+        
+            let origin:DPlanStop = travel.origin,
+            let destination:DPlanStop = travel.destination
+        
+        else
+        {
+            return nil
+        }
+        
+        let itemOrigin:MKMapItem = factoryMapItem(
+            stop:origin)
+        let itemDestination:MKMapItem = factoryMapItem(
+            stop:destination)
+        
+    }
+    
+    private func factoryMapItem(
+        stop:DPlanStop) -> MKMapItem
+    {
+        let placemark:MKPlacemark = MKPlacemark(
+            coordinate:stop.coordinate,
+            addressDictionary:nil)
+        let mapItem:MKMapItem = MKMapItem(
+            placemark:placemark)
+        
+        return mapItem
+    }
+    
     //MARK: internal
     
     func directions(
