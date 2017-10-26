cc076003caf3c824b70c611271882fb3987fecb6
vauxhall <zero.griffin@gmail.com>
2017-10-26 18:42:08 -0500

    Layout subviews

--- Source/View/Create/VCreateStatusReadyBarStopsFooter.swift
+++ Source/View/Create/VCreateStatusReadyBarStopsFooter.swift
@@ -3,6 +3,17 @@ import UIKit
 final class VCreateStatusReadyBarStopsFooter:
     UICollectionReusableView
 {
+    weak var labelDistanceTitle:UILabel!
+    weak var labelDistanceValue:UILabel!
+    weak var labelDurationTitle:UILabel!
+    weak var labelDurationValue:UILabel!
+    let kFontSize:CGFloat = 14
+    let kTitleLeft:CGFloat = 10
+    let kTitleWidth:CGFloat = 100
+    let kValueWidth:CGFloat = 200
+    let kDistanceTop:CGFloat = 20
+    let kLabelHeight:CGFloat = 24
+    
     override init(frame:CGRect)
     {
         super.init(frame:frame)
--- Source/View/Create/VCreateStatusReadyBarStopsFooterFactory.swift
+++ Source/View/Create/VCreateStatusReadyBarStopsFooterFactory.swift
@@ -4,4 +4,97 @@ extension VCreateStatusReadyBarStopsFooter
 {
     //MARK: internal
     
+    func factoryViews()
+    {
+        let labelDistanceTitle:UILabel = UILabel()
+        labelDistanceTitle.isUserInteractionEnabled = false
+        labelDistanceTitle.translatesAutoresizingMaskIntoConstraints = false
+        labelDistanceTitle.backgroundColor = UIColor.clear
+        labelDistanceTitle.font = UIFont.medium(size:kFontSize)
+        labelDistanceTitle.textColor = UIColor.white
+        self.labelDistanceTitle = labelDistanceTitle
+        
+        let labelDistanceValue:UILabel = UILabel()
+        labelDistanceValue.isUserInteractionEnabled = false
+        labelDistanceValue.translatesAutoresizingMaskIntoConstraints = false
+        labelDistanceValue.backgroundColor = UIColor.clear
+        labelDistanceValue.font = UIFont.regular(size:kFontSize)
+        labelDistanceValue.textColor = UIColor.white
+        self.labelDistanceValue = labelDistanceValue
+        
+        let labelDurationTitle:UILabel = UILabel()
+        labelDurationTitle.isUserInteractionEnabled = false
+        labelDurationTitle.translatesAutoresizingMaskIntoConstraints = false
+        labelDurationTitle.backgroundColor = UIColor.clear
+        labelDurationTitle.font = UIFont.medium(size:kFontSize)
+        labelDurationTitle.textColor = UIColor.white
+        self.labelDurationTitle = labelDurationTitle
+        
+        let labelDurationValue:UILabel = UILabel()
+        labelDurationValue.isUserInteractionEnabled = false
+        labelDurationValue.translatesAutoresizingMaskIntoConstraints = false
+        labelDurationValue.backgroundColor = UIColor.clear
+        labelDurationValue.font = UIFont.regular(size:kFontSize)
+        labelDurationValue.textColor = UIColor.white
+        self.labelDurationValue = labelDurationValue
+        
+        addSubview(labelDistanceTitle)
+        addSubview(labelDurationTitle)
+        addSubview(labelDistanceValue)
+        addSubview(labelDurationValue)
+        
+        NSLayoutConstraint.topToTop(
+            view:labelDistanceTitle,
+            toView:self)
+        NSLayoutConstraint.height(
+            view:labelDistanceTitle,
+            constant:kLabelHeight)
+        NSLayoutConstraint.leftToLeft(
+            view:labelDistanceTitle,
+            toView:self,
+            constant:kTitleLeft)
+        NSLayoutConstraint.width(
+            view:labelDistanceTitle,
+            constant:kTitleWidth)
+        
+        NSLayoutConstraint.topToTop(
+            view:labelDistanceValue,
+            toView:self)
+        NSLayoutConstraint.height(
+            view:labelDistanceValue,
+            constant:kLabelHeight)
+        NSLayoutConstraint.leftToRight(
+            view:labelDistanceValue,
+            toView:labelDistanceTitle)
+        NSLayoutConstraint.width(
+            view:labelDistanceValue,
+            constant:kValueWidth)
+        
+        NSLayoutConstraint.topToBottom(
+            view:labelDurationTitle,
+            toView:labelDistanceTitle)
+        NSLayoutConstraint.height(
+            view:labelDurationTitle,
+            constant:kLabelHeight)
+        NSLayoutConstraint.leftToLeft(
+            view:labelDurationTitle,
+            toView:self,
+            constant:kTitleLeft)
+        NSLayoutConstraint.width(
+            view:labelDurationTitle,
+            constant:kTitleWidth)
+        
+        NSLayoutConstraint.topToBottom(
+            view:labelDurationValue,
+            toView:labelDistanceValue)
+        NSLayoutConstraint.height(
+            view:labelDurationValue,
+            constant:kLabelHeight)
+        NSLayoutConstraint.leftToRight(
+            view:labelDurationValue,
+            toView:labelDurationTitle)
+        NSLayoutConstraint.width(
+            view:labelDurationValue,
+            constant:kTitleWidth)
+    }
 }
