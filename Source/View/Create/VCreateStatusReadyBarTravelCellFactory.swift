8c81bbf983424aab01548d0595af395769f61aff
vauxhall <zero.griffin@gmail.com>
2017-10-26 16:28:29 -0500

    Create factory extension

---
+++ Source/View/Create/VCreateStatusReadyBarTravelCellFactory.swift
@@ -0,0 +1,17 @@
+import UIKit
+
+extension VCreateStatusReadyBarTravelCell
+{
+    //MARK: internal
+    
+    class func factoryImage(
+        mode:DPlanTravelMode) -> UIImage?
+    {
+        
+    }
+    
+    func factoryViews()
+    {
+        
+    }
+}
