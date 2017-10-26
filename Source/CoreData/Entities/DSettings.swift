93d25a828a4706965bdf3c7e51f0cd14fc7ed847
vauxhall <zero.griffin@gmail.com>
2017-10-26 05:21:25 -0500

    Create computed extension

--- Source/CoreData/Entities/DSettings.swift
+++ Source/CoreData/Entities/DSettings.swift
@@ -2,29 +2,6 @@ import Foundation
 
 extension DSettings
 {
-    var travelMode:DPlanTravelMode
-    {
-        get
-        {
-            guard
-                
-                let travelMode:DPlanTravelMode = DPlanTravelMode(
-                    rawValue:rawTravelMode)
-                
-            else
-            {
-                return DPlanTravelMode.unknown
-            }
-            
-            return travelMode
-        }
-        
-        set(newValue)
-        {
-            rawTravelMode = newValue.rawValue
-        }
-    }
-    
     public override func awakeFromInsert()
     {
         travelMode = DPlanTravelMode.walking
---
+++ Source/CoreData/Entities/DSettingsComputed.swift
@@ -0,0 +1,27 @@
+import Foundation
+
+extension DSettings
+{
+    var travelMode:DPlanTravelMode
+    {
+        get
+        {
+            guard
+                
+                let travelMode:DPlanTravelMode = DPlanTravelMode(
+                    rawValue:rawTravelMode)
+                
+            else
+            {
+                return DPlanTravelMode.unknown
+            }
+            
+            return travelMode
+        }
+        
+        set(newValue)
+        {
+            rawTravelMode = newValue.rawValue
+        }
+    }
+}
