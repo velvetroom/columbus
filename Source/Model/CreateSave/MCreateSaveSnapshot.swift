ad597dcc7d17242af34943a09377ffc266fefb1a
vauxhall <zero.griffin@gmail.com>
2017-10-27 18:50:13 -0500

    Factory snapshots

--- Source/Model/CreateSave/MCreateSaveSnapshot.swift
+++ Source/Model/CreateSave/MCreateSaveSnapshot.swift
@@ -2,6 +2,65 @@ import MapKit
 
 extension MCreateSave
 {
+    //MARK: private
+    
+    private class func snapshots(
+        dispatchGroup:DispatchGroup,
+        options:[MKMapSnapshotOptions],
+        completion:@escaping(([UIImage]) -> ()))
+    {
+        var images:[UIImage] = []
+        
+        for option:MKMapSnapshotOptions in options
+        {
+            dispatchGroup.enter()
+            
+            snapshot(option:option)
+            { (image:UIImage?) in
+                
+                if let image:UIImage = image
+                {
+                    images.append(image)
+                }
+                
+                dispatchGroup.leave()
+            }
+        }
+        
+        dispatchGroup.notify(
+            queue:DispatchQueue.global(
+                qos:DispatchQoS.QoSClass.background))
+        {
+            completion(images)
+        }
+    }
+    
+    private class func snapshot(
+        option:MKMapSnapshotOptions,
+        completion:@escaping((UIImage?) -> ()))
+    {
+        let snapshotter:MKMapSnapshotter = MKMapSnapshotter(
+            options:option)
+        
+        snapshotter.start
+        { (snapshot:MKMapSnapshot?, error:Error?) in
+            
+            guard
+            
+                error == nil,
+                let image:UIImage = snapshot?.image
+            
+            else
+            {
+                completion(nil)
+                
+                return
+            }
+            
+            completion(image)
+        }
+    }
+    
     //MARK: internal
     
     class func snapshots(
@@ -10,5 +69,11 @@ extension MCreateSave
     {
         let options:[MKMapSnapshotOptions] = factorySnapshotOptions(
             mapRange:mapRange)
+        let dispatchGroup:DispatchGroup = MCreateSave.factoryDispatchGroup()
+        
+        snapshots(
+            dispatchGroup:dispatchGroup,
+            options:options,
+            completion:completion)
     }
 }
