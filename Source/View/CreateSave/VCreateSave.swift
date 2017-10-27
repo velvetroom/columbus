caf0d752c75c9877a16adb0bd1d1e3cdc35e789d
vauxhall <zero.griffin@gmail.com>
2017-10-27 11:06:56 -0500

    Factory views

--- Source/View/CreateSave/VCreateSave.swift
+++ Source/View/CreateSave/VCreateSave.swift
@@ -2,5 +2,36 @@ import UIKit
 
 final class VCreateSave:ViewMain
 {
+    weak var viewSpinner:VSpinner!
+    let kTitleBottom:CGFloat = -150
+    let kTitleHeight:CGFloat = 24
+    let kButtonHeight:CGFloat = 50
+    let kTitleFontSize:CGFloat = 16
+    let kButtonFontSize:CGFloat = 16
     
+    required init(controller:UIViewController)
+    {
+        super.init(controller:controller)
+        
+        guard
+        
+            let controller:CCreateSave = controller as? CCreateSave
+        
+        else
+        {
+            return
+        }
+        
+        factoryViews(controller:controller)
+    }
+    
+    required init?(coder:NSCoder)
+    {
+        return nil
+    }
+    
+    deinit
+    {
+        viewSpinner.stopAnimating()
+    }
 }
--- Source/View/CreateSave/VCreateSaveFactory.swift
+++ Source/View/CreateSave/VCreateSaveFactory.swift
@@ -6,6 +6,22 @@ extension VCreateSave
     
     func factoryViews(controller:CCreateSave)
     {
+        let viewGradient:VGradient = VGradient.vertical(
+            colourTop:UIColor.colourGradientLight,
+            colourBottom:UIColor.colourGradientDark)
         
+        let viewSpinner:VSpinner = VSpinner()
+        self.viewSpinner = viewSpinner
+        
+        addSubview(viewGradient)
+        addSubview(viewSpinner)
+        
+        NSLayoutConstraint.equals(
+            view:viewGradient,
+            toView:self)
+        
+        NSLayoutConstraint.equals(
+            view:viewSpinner,
+            toView:self)
     }
 }
