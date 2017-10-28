c8ef752058a4379aa5544f367c23dadaf496c708
vauxhall <zero.griffin@gmail.com>
2017-10-27 19:05:29 -0500

    Factory options

--- Source/Model/CreateSave/MCreateSaveSnapshotFactory.swift
+++ Source/Model/CreateSave/MCreateSaveSnapshotFactory.swift
@@ -7,6 +7,14 @@ extension MCreateSave
     class func factorySnapshotOptions(
         mapRange:MCreateSaveMapRange) -> [MKMapSnapshotOptions]
     {
+        let mapRect:MKMapRect = mapRange.factoryMapRect()
         
+        let option:MKMapSnapshotOptions = MKMapSnapshotOptions()
+        option.mapRect = mapRect
+        
+        let options:[MKMapSnapshotOptions] = [
+            option]
+        
+        return options
     }
 }
