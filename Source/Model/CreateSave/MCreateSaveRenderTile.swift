13a51b9274e82c7e0468d68e853eca530a676189
vauxhall <zero.griffin@gmail.com>
2017-10-28 19:16:02 -0500

    Factory tile name

--- Source/Model/CreateSave/MCreateSaveRenderTile.swift
+++ Source/Model/CreateSave/MCreateSaveRenderTile.swift
@@ -4,6 +4,6 @@ struct MCreateSaveRenderTile
 {
     let x:Int
     let y:Int
-    let options:MKMapSnapshotOptions
     let name:String
+    let options:MKMapSnapshotOptions
 }
--- Source/Model/CreateSave/MCreateSaveTileRangeFactory.swift
+++ Source/Model/CreateSave/MCreateSaveTileRangeFactory.swift
@@ -2,7 +2,64 @@ import MapKit
 
 extension MCreateSaveTileRange
 {
+    private static let kNameSeparator:String = "."
+    
+    //MARK: private
+    
+    private func factoryTiles() -> [MCreateSaveRenderTile]
+    {
+        var tiles:[MCreateSaveRenderTile] = []
+        
+        let minX:Int = Int(self.minX)
+        let maxX:Int = Int(self.maxX)
+        let minY:Int = Int(self.minY)
+        let maxY:Int = Int(self.maxY)
+        
+        for indexX:Int in minX ... maxX
+        {
+            for indexY:Int in minY ... maxY
+            {
+                let tile:MCreateSaveRenderTile = factoryTile(
+                    indexX:indexX,
+                    indexY:indexY)
+                
+                tiles.append(tile)
+            }
+        }
+        
+        return tiles
+    }
+    
+    private func factoryTile(
+        indexX:Int,
+        indexY:Int) -> MCreateSaveRenderTile
+    {
+        
+    }
+    
+    private func factoryName(
+        indexX:Int,
+        indexY:Int) -> String
+    {
+        let zoomInt:Int = Int(zoom)
+        let stringX:String = String(indexX)
+        let stringY:String = String(indexY)
+        let stringZ:String = String(zoomInt)
+        
+        var string:String = String()
+        string.append(stringX)
+        string.append(MCreateSaveTileRange.kNameSeparator)
+        string.append(stringY)
+        string.append(MCreateSaveTileRange.kNameSeparator)
+        string.append(stringZ)
+        
+        return string
+    }
+    
     //MARK: internal
     
-    
+    func factoryRender() -> MCreateSaveRender
+    {
+        
+    }
 }
