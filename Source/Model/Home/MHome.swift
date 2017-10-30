4b5de33da490293bee9c42c65ddc59fa4ce51d0a
vauxhall <zero.griffin@gmail.com>
2017-10-30 07:47:20 -0600

    Add update status

--- Source/Model/Home/MHome.swift
+++ Source/Model/Home/MHome.swift
@@ -4,4 +4,16 @@ final class MHome:Model<ArchHome>
 {
     var database:Database?
     var settings:DSettings?
+    private(set) var status:MHomeStatusProtocol?
+    
+    //MARK: internal
+    
+    func changeStatus(
+        statusType:MHomeStatusProtocol.Type)
+    {
+        let status:MHomeStatusProtocol = statusType.init()
+        self.status = status
+        
+        view?.updateStatus()
+    }
 }
--- Source/View/Home/VHome.swift
+++ Source/View/Home/VHome.swift
@@ -2,5 +2,43 @@ import UIKit
 
 final class VHome:ViewMain
 {
+    private(set) weak var view:View<ArchHome>?
     
+    //MARK: private
+    
+    private func asyncUpdateStatus()
+    {
+        self.view?.removeFromSuperview()
+        
+        guard
+            
+            let controller:CHome = self.controller as? CHome,
+            let status:MHomeStatusProtocol = controller.model.status
+            
+        else
+        {
+            return
+        }
+        
+        let view:View<ArchHome> = status.viewType.init(
+            controller:controller)
+        self.view = view
+        
+        addSubview(view)
+        
+        NSLayoutConstraint.equals(
+            view:view,
+            toView:self)
+    }
+    
+    //MARK: internal
+    
+    func updateStatus()
+    {
+        DispatchQueue.main.async
+        { [weak self] in
+            
+            self?.asyncUpdateStatus()
+        }
+    }
 }
