b8863f21a2e2b416b6df6b37992f243afa076492
vauxhall <zero.griffin@gmail.com>
2017-10-31 17:52:19 -0600

    Layout view plans list

--- Source/View/Plans/VPlansList.swift
+++ Source/View/Plans/VPlansList.swift
@@ -4,5 +4,19 @@ final class VPlansList:VCollection<
     ArchPlans,
     VPlansListCell>
 {
+    let kInterItem:CGFloat = 4
+    let kInsetsTop:CGFloat = 63
+    let kInsetsBottom:CGFloat = 70
     
+    required init(controller:CPlans)
+    {
+        super.init(controller:controller)
+        
+        config()
+    }
+    
+    required init?(coder:NSCoder)
+    {
+        return nil
+    }
 }
--- Source/View/Plans/VPlansListConfig.swift
+++ Source/View/Plans/VPlansListConfig.swift
@@ -6,6 +6,21 @@ extension VPlansList
     
     func config()
     {
+        backgroundColor = UIColor.colourBackgroundGray
+        collectionView.alwaysBounceVertical = true
         
+        registerCell(
+            cell:VSettingsListCellTravelMode.self)
+        
+        if let flow:VCollectionFlow = collectionView.collectionViewLayout as? VCollectionFlow
+        {
+            flow.minimumInteritemSpacing = kInterItem
+            flow.minimumLineSpacing = kInterItem
+            flow.sectionInset = UIEdgeInsets(
+                top:kInsetsTop,
+                left:0,
+                bottom:kInsetsBottom,
+                right:0)
+        }
     }
 }
