c9fa8e2c3785c31e7ecee6d8c32833d1a4410408
vauxhall <zero.griffin@gmail.com>
2017-10-31 19:30:50 -0600

    Layout labels route

--- Source/View/Plans/VPlansListCell.swift
+++ Source/View/Plans/VPlansListCell.swift
@@ -2,8 +2,14 @@ import UIKit
 
 final class VPlansListCell:UICollectionViewCell
 {
+    weak var labelOrigin:UILabel!
+    weak var labelDestination:UILabel!
     let kRouteWidth:CGFloat = 60
     let kRouteHeight:CGFloat = 80
+    let kLabelRouteTop:CGFloat = 20
+    let kLabelRouteHeight:CGFloat = 20
+    let kLabelRouteRight:CGFloat = -10
+    let kRouteFontSize:CGFloat = 14
     
     override init(frame:CGRect)
     {
--- Source/View/Plans/VPlansListCellFactory.swift
+++ Source/View/Plans/VPlansListCellFactory.swift
@@ -13,7 +13,16 @@ extension VPlansListCell
         imageRoute.contentMode = UIViewContentMode.center
         imageRoute.image = #imageLiteral(resourceName: "assetPlansRoute")
         
+        let labelOrigin:UILabel = UILabel()
+        labelOrigin.isUserInteractionEnabled = false
+        labelOrigin.translatesAutoresizingMaskIntoConstraints = false
+        labelOrigin.backgroundColor = UIColor.clear
+        labelOrigin.font = UIFont.light(size:kRouteFontSize)
+        labelOrigin.textColor = UIColor.colourBackgroundDark
+        self.labelOrigin = labelOrigin
+        
         addSubview(imageRoute)
+        addSubview(labelOrigin)
         
         NSLayoutConstraint.topToTop(
             view:imageRoute,
@@ -27,5 +36,34 @@ extension VPlansListCell
         NSLayoutConstraint.height(
             view:imageRoute,
             constant:kRouteHeight)
+        
+        NSLayoutConstraint.topToTop(
+            view:labelOrigin,
+            toView:self,
+            constant:kLabelRouteTop)
+        NSLayoutConstraint.height(
+            view:labelOrigin,
+            constant:kLabelRouteHeight)
+        NSLayoutConstraint.leftToRight(
+            view:labelOrigin,
+            toView:imageRoute)
+        NSLayoutConstraint.rightToRight(
+            view:labelOrigin,
+            toView:self,
+            constant:kLabelRouteRight)
+        
+        NSLayoutConstraint.topToBottom(
+            view:labelDestination,
+            toView:labelOrigin)
+        NSLayoutConstraint.height(
+            view:labelDestination,
+            constant:kLabelRouteHeight)
+        NSLayoutConstraint.leftToRight(
+            view:labelDestination,
+            toView:imageRoute)
+        NSLayoutConstraint.rightToRight(
+            view:labelDestination,
+            toView:self,
+            constant:kLabelRouteRight)
     }
 }
