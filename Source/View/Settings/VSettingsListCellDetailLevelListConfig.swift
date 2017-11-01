60b496c506a55f7060f27ffc88df6e0fcbbfc0c5
vauxhall <zero.griffin@gmail.com>
2017-11-01 17:07:24 -0600

    Create list config extension

---
+++ Source/View/Settings/VSettingsListCellDetailLevelListConfig.swift
@@ -0,0 +1,34 @@
+import UIKit
+
+extension VSettingsListCellDetailLevelList
+{
+    //MARK: internal
+    
+    func config()
+    {
+        collectionView.isScrollEnabled = false
+        collectionView.bounces = false
+        
+        if let flow:VCollectionFlow = collectionView.collectionViewLayout as? VCollectionFlow
+        {
+            flow.scrollDirection = UICollectionViewScrollDirection.vertical
+        }
+        
+        let viewSelector:VSettingsListCellTravelModeListSelector = VSettingsListCellTravelModeListSelector()
+        self.viewSelector = viewSelector
+        
+        insertSubview(
+            viewSelector,
+            belowSubview:collectionView)
+        
+        layoutSelectorTop = NSLayoutConstraint.topToTop(
+            view:viewSelector,
+            toView:self)
+        layoutSelectorLeft = NSLayoutConstraint.leftToLeft(
+            view:viewSelector,
+            toView:self)
+        NSLayoutConstraint.size(
+            view:viewSelector,
+            constant:kSelectorSize)
+    }
+}
