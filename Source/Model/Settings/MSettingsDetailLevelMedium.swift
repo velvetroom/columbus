7ab4395334fe9f6295cdbb9387fb4c24bd4c7fdc
vauxhall <zero.griffin@gmail.com>
2017-11-01 11:47:06 -0600

    Create struct detail medium

---
+++ Source/Model/Settings/MSettingsDetailLevelMedium.swift
@@ -0,0 +1,10 @@
+import Foundation
+
+struct MSettingsDetailLevelMedium:MSettingsDetailLevelProtocol
+{
+    let detailLevel:DSettingsDetailLevel = DSettingsDetailLevel.medium
+    let title:String = String.localizedModel(
+        key:"MSettingsDetailLevelMedium_title")
+    let descr:String = String.localizedModel(
+        key:"MSettingsDetailLevelMedium_descr")
+}
