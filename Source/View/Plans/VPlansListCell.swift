bdd10fa9cc312cad13bd4bdfbd7641aa925998a2
vauxhall <zero.griffin@gmail.com>
2017-10-31 19:18:30 -0600

    Layout icon route

--- Source/View/Plans/VPlansListCell.swift
+++ Source/View/Plans/VPlansListCell.swift
@@ -2,11 +2,16 @@ import UIKit
 
 final class VPlansListCell:UICollectionViewCell
 {
+    let kRouteWidth:CGFloat = 60
+    let kRouteHeight:CGFloat = 80
+    
     override init(frame:CGRect)
     {
         super.init(frame:frame)
         backgroundColor = UIColor.white
         clipsToBounds = true
+        
+        factoryViews()
     }
     
     required init?(coder:NSCoder)
--- Source/View/Plans/VPlansListCellFactory.swift
+++ Source/View/Plans/VPlansListCellFactory.swift
@@ -4,5 +4,28 @@ extension VPlansListCell
 {
     //MARK: internal
     
-    
+    func factoryViews()
+    {
+        let imageRoute:UIImageView = UIImageView()
+        imageRoute.isUserInteractionEnabled = false
+        imageRoute.translatesAutoresizingMaskIntoConstraints = false
+        imageRoute.clipsToBounds = true
+        imageRoute.contentMode = UIViewContentMode.center
+        imageRoute.image = #imageLiteral(resourceName: "assetPlansRoute")
+        
+        addSubview(imageRoute)
+        
+        NSLayoutConstraint.topToTop(
+            view:imageRoute,
+            toView:self)
+        NSLayoutConstraint.leftToLeft(
+            view:imageRoute,
+            toView:self)
+        NSLayoutConstraint.width(
+            view:imageRoute,
+            constant:kRouteWidth)
+        NSLayoutConstraint.height(
+            view:imageRoute,
+            constant:kRouteHeight)
+    }
 }
