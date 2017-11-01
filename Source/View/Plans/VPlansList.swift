a8114aa40bb5ea04690ffd1654c53e4123681a9b
vauxhall <zero.griffin@gmail.com>
2017-10-31 18:53:40 -0600

    Add model at index

--- Source/View/Plans/VPlansList.swift
+++ Source/View/Plans/VPlansList.swift
@@ -7,6 +7,8 @@ final class VPlansList:VCollection<
     let kInterItem:CGFloat = 4
     let kInsetsTop:CGFloat = 63
     let kInsetsBottom:CGFloat = 70
+    private var cellSize:CGSize?
+    private let kCellHeight:CGFloat = 120
     
     required init(controller:CPlans)
     {
@@ -20,5 +22,51 @@ final class VPlansList:VCollection<
         return nil
     }
     
+    override func collectionView(
+        _ collectionView:UICollectionView,
+        numberOfItemsInSection section:Int) -> Int
+    {
+        guard
+        
+            let count:Int = controller.model.plans?.count
+        
+        else
+        {
+            return 0
+        }
+        
+        return count
+    }
     
+    override func collectionView(
+        _ collectionView:UICollectionView,
+        layout collectionViewLayout:UICollectionViewLayout,
+        sizeForItemAt indexPath:IndexPath) -> CGSize
+    {
+        guard
+        
+            let cellSize:CGSize = self.cellSize
+        
+        else
+        {
+            let width:CGFloat = collectionView.bounds.width
+            let cellSize:CGSize = CGSize(
+                width:width,
+                height:kCellHeight)
+            self.cellSize = cellSize
+            
+            return cellSize
+        }
+        
+        return cellSize
+    }
+    
+    override func collectionView(
+        _ collectionView:UICollectionView,
+        cellForItemAt indexPath:IndexPath) -> UICollectionViewCell
+    {
+        let cell:VPlansListCell = cellAtIndex(indexPath:indexPath)
+        
+        return cell
+    }
 }
--- Source/View/Plans/VPlansListActions.swift
+++ Source/View/Plans/VPlansListActions.swift
@@ -4,5 +4,11 @@ extension VPlansList
 {
     //MARK: internal
     
-    
+    func modelAtIndex(index:IndexPath) -> DPlan
+    {
+        let item:DPlan = controller.model.plans![
+            index.item]
+        
+        return item
+    }
 }
