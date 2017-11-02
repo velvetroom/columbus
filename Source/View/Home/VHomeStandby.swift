209b7201332db7f20d0575f2a5a0f9286695414a
vauxhall <zero.griffin@gmail.com>
2017-11-02 13:43:52 -0600

    Add list cell

--- Source/View/Home/VHomeStandby.swift
+++ Source/View/Home/VHomeStandby.swift
@@ -9,6 +9,8 @@ final class VHomeStandby:VCollection<
     let kHeaderMinHeight:CGFloat = 20
     let kInterItem:CGFloat = 1
     let kCollectionBottom:CGFloat = 70
+    private var cellSize:CGSize?
+    private let kCellHeight:CGFloat = 100
     
     required init(controller:CHome)
     {
@@ -34,5 +36,37 @@ final class VHomeStandby:VCollection<
         }
         
         layoutHeaderHeight.constant = headerHeight
+    }
+    
+    override func collectionView(
+        _ collectionView:UICollectionView,
+        layout collectionViewLayout:UICollectionViewLayout,
+        sizeForItemAt indexPath:IndexPath) -> CGSize
+    {
+        guard
+        
+            let cellSize:CGSize = self.cellSize
+        
+        else
+        {
+            let width:CGFloat = collectionView.bounds.width
+            let cellSize:CGSize = CGSize(
+                width:width,
+                height:kCellHeight)
+            self.cellSize = cellSize
+            
+            return cellSize
+        }
+        
+        return cellSize
+    }
+    
+    override func collectionView(
+        _ collectionView:UICollectionView,
+        numberOfItemsInSection section:Int) -> Int
+    {
+        let count:Int = controller.model.info.count
+        
+        return count
     }
 }
