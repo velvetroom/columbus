3d76df2e73f530f1752b9b4423d6cbfa5f4ea05b
vauxhall <zero.griffin@gmail.com>
2017-11-02 12:38:38 -0600

    Add header to list

--- Source/View/Home/VHomeStandby.swift
+++ Source/View/Home/VHomeStandby.swift
@@ -4,5 +4,29 @@ final class VHomeStandby:VCollection<
     ArchHome,
     VHomeStandbyCell>
 {
+    let kHeaderHeight:CGFloat = 300
+    let kInterItem:CGFloat = 1
     
+    required init(controller:CHome)
+    {
+        super.init(controller:controller)
+        config()
+    }
+    
+    required init?(coder:NSCoder)
+    {
+        return nil
+    }
+    
+    override func collectionView(
+        _ collectionView:UICollectionView,
+        viewForSupplementaryElementOfKind kind:String,
+        at indexPath:IndexPath) -> UICollectionReusableView
+    {
+        let header:VHomeStandbyHeader = reusableAtIndex(
+            kind:kind,
+            indexPath:indexPath)
+        
+        return header
+    }
 }
--- Source/View/Home/VHomeStandbyConfig.swift
+++ Source/View/Home/VHomeStandbyConfig.swift
@@ -3,4 +3,20 @@ import UIKit
 extension VHomeStandby
 {
     //MARK: internal
+    
+    func config()
+    {
+        backgroundColor = UIColor.colourBackgroundGray
+        
+        collectionView.alwaysBounceVertical = true
+        
+        if let flow:VCollectionFlow = collectionView.collectionViewLayout as? VCollectionFlow
+        {
+            flow.headerReferenceSize = CGSize(
+                width:0,
+                height:kHeaderHeight)
+            flow.minimumLineSpacing = kInterItem
+            flow.minimumInteritemSpacing = kInterItem
+        }
+    }
 }
