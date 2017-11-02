3c96a02aa0747675d1d9be68dd11da431ba5ead9
vauxhall <zero.griffin@gmail.com>
2017-11-01 22:08:26 -0600

    Add views factory

--- Source/View/Settings/VSettingsListCellDetailLevelList.swift
+++ Source/View/Settings/VSettingsListCellDetailLevelList.swift
@@ -24,6 +24,7 @@ final class VSettingsListCellDetailLevelList:
         
         super.init(controller:controller)
         config()
+        factoryViews()
         selectCurrent()
     }
     
--- Source/View/Settings/VSettingsListCellDetailLevelListConfig.swift
+++ Source/View/Settings/VSettingsListCellDetailLevelListConfig.swift
@@ -13,44 +13,5 @@ extension VSettingsListCellDetailLevelList
         {
             flow.scrollDirection = UICollectionViewScrollDirection.vertical
         }
-        
-        let viewSelector:VSettingsListCellDetailLevelListSelector = VSettingsListCellDetailLevelListSelector()
-        self.viewSelector = viewSelector
-        
-        let viewRail:VSettingsListCellDetailLevelListRail = VSettingsListCellDetailLevelListRail()
-        
-        insertSubview(
-            viewRail,
-            belowSubview:collectionView)
-        
-        insertSubview(
-            viewSelector,
-            belowSubview:collectionView)
-        
-        layoutSelectorTop = NSLayoutConstraint.topToTop(
-            view:viewSelector,
-            toView:self)
-        NSLayoutConstraint.height(
-            view:viewSelector,
-            constant:kSelectorHeight)
-        NSLayoutConstraint.leftToLeft(
-            view:viewSelector,
-            toView:self,
-            constant:kSelectorMarginHorizontal)
-        NSLayoutConstraint.equalsHorizontal(
-            view:viewSelector,
-            toView:self,
-            margin:kSelectorMarginHorizontal)
-        
-        NSLayoutConstraint.equalsVertical(
-            view:viewRail,
-            toView:self)
-        NSLayoutConstraint.leftToLeft(
-            view:viewRail,
-            toView:self,
-            constant:kRailLeft)
-        NSLayoutConstraint.width(
-            view:viewRail,
-            constant:kRailWidth)
     }
 }
