afbb3e8dc3b092521d46d22ccc43d75aa62b263e
vauxhall <zero.griffin@gmail.com>
2017-11-02 16:49:11 -0600

    Factory views

--- Source/View/Home/VHomeStandbyCell.swift
+++ Source/View/Home/VHomeStandbyCell.swift
@@ -2,11 +2,21 @@ import UIKit
 
 final class VHomeStandbyCell:UICollectionViewCell
 {
+    weak var icon:UIImageView!
+    weak var labelInfo:UILabel!
+    weak var layoutInfoHeight:NSLayoutConstraint!
+    let kIconWidth:CGFloat = 90
+    let kInfoTop:CGFloat = 10
+    let kInfoRight:CGFloat = -10
+    let kInfoMaxHeight:CGFloat = 200
+    
     override init(frame:CGRect)
     {
         super.init(frame:frame)
         clipsToBounds = true
         backgroundColor = UIColor.white
+        
+        factoryViews()
     }
     
     required init?(coder:NSCoder)
--- Source/View/Home/VHomeStandbyCellFactory.swift
+++ Source/View/Home/VHomeStandbyCellFactory.swift
@@ -3,4 +3,44 @@ import UIKit
 extension VHomeStandbyCell
 {
     //MARK: internal
+    
+    func factoryViews()
+    {
+        let icon:UIImageView = UIImageView()
+        self.icon = icon
+        
+        let labelInfo:UILabel = UILabel()
+        labelInfo.isUserInteractionEnabled = false
+        labelInfo.translatesAutoresizingMaskIntoConstraints = false
+        labelInfo.backgroundColor = UIColor.clear
+        labelInfo.numberOfLines = 0
+        self.labelInfo = labelInfo
+        
+        addSubview(icon)
+        addSubview(labelInfo)
+        
+        NSLayoutConstraint.equalsVertical(
+            view:icon,
+            toView:self)
+        NSLayoutConstraint.leftToLeft(
+            view:icon,
+            toView:self)
+        NSLayoutConstraint.width(
+            view:icon,
+            constant:kIconWidth)
+        
+        NSLayoutConstraint.topToTop(
+            view:labelInfo,
+            toView:self,
+            constant:kInfoTop)
+        layoutInfoHeight = NSLayoutConstraint.height(
+            view:labelInfo)
+        NSLayoutConstraint.leftToRight(
+            view:labelInfo,
+            toView:icon)
+        NSLayoutConstraint.rightToRight(
+            view:labelInfo,
+            toView:self,
+            constant:kInfoRight)
+    }
 }
