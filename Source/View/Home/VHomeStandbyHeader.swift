c52bea2eb24c9a0ad52e891ceef8446480ec552f
vauxhall <zero.griffin@gmail.com>
2017-11-02 11:52:47 -0600

    Drop list from view's name

---
+++ Source/View/Home/VHomeStandbyHeader.swift
@@ -0,0 +1,6 @@
+import UIKit
+
+final class VHomeStandbyHeader:UICollectionReusableView
+{
+    
+}
--- Source/View/Home/VHomeStandbyListHeader.swift
+++
@@ -1,6 +0,0 @@
-import UIKit
-
-final class VHomeStandbyListHeader:UICollectionReusableView
-{
-    
-}
