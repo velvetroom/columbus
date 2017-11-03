29441b9d0f09b1bae54a37234ecf452f8419f545
vauxhall <zero.griffin@gmail.com>
2017-11-03 10:19:18 -0600

    Factory travels duration

--- Source/CoreData/Entities/DPlanTravelDuration.swift
+++ Source/CoreData/Entities/DPlanTravelDuration.swift
@@ -3,4 +3,17 @@ import Foundation
 extension DPlanTravel
 {
     //MARK: internal
+    
+    class func factoryDuration(
+        travels:[DPlanTravel]) -> TimeInterval
+    {
+        var duration:TimeInterval = 0
+        
+        for travel:DPlanTravel in travels
+        {
+            duration += travel.duration
+        }
+        
+        return duration
+    }
 }
