93922c616a3313f0b8ddb7f81ebc603c3023453e
vauxhall <zero.griffin@gmail.com>
2017-11-03 19:10:52 -0600

    Add activate/deactivate plan functionality

--- Source/Model/Plans/MPlans.swift
+++ Source/Model/Plans/MPlans.swift
@@ -6,4 +6,42 @@ final class MPlans:Model<ArchPlans>
     var settings:DSettings?
     var plans:[DPlan]?
     let kSortersKey:String = "created"
+    
+    //MARK: private
+    
+    private func activate(
+        settings:DSettings,
+        plan:DPlan)
+    {
+        guard
+            
+            settings.activePlan == plan
+            
+        else
+        {
+            settings.activePlan = plan
+            
+            return
+        }
+        
+        settings.activePlan = nil
+    }
+    
+    //MARK: internal
+    
+    func activate(plan:DPlan)
+    {
+        guard
+        
+            let settings:DSettings = self.settings
+        
+        else
+        {
+            return
+        }
+        
+        activate(
+            settings:settings,
+            plan:plan)
+    }
 }
--- Source/View/Plans/VPlansListCellButton.swift
+++ Source/View/Plans/VPlansListCellButton.swift
@@ -2,5 +2,79 @@ import UIKit
 
 final class VPlansListCellButton:View<ArchPlans>
 {
+    private weak var button:UIButton!
+    private weak var model:DPlan?
     
+    required init(controller:CPlans)
+    {
+        super.init(controller:controller)
+        
+        let button:UIButton = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.imageView!.clipsToBounds = true
+        button.imageView!.contentMode = UIViewContentMode.center
+        button.addTarget(
+            self,
+            action:#selector(selectorButton(button:)),
+            for:UIControlEvents.touchUpInside)
+        self.button = button
+        
+        addSubview(button)
+        
+        NSLayoutConstraint.equals(
+            view:button,
+            toView:self)
+    }
+    
+    required init?(coder:NSCoder)
+    {
+        return nil
+    }
+    
+    //MARK: selectors
+    
+    @objc
+    private func selectorButton(button sender:UIButton)
+    {
+        guard
+        
+            let model:DPlan = self.model
+        
+        else
+        {
+            return
+        }
+        
+        controller.model.activate(
+            plan:model)
+    }
+    
+    //MARK: private
+    
+    private func updateImage(
+        image:UIImage)
+    {
+        button.setImage(
+            image,
+            for:UIControlState.normal)
+        button.setImage(
+            image,
+            for:UIControlState.highlighted)
+    }
+    
+    //MARK: internal
+    
+    func active(
+        model:DPlan)
+    {
+        self.model = model
+        updateImage(image:#imageLiteral(resourceName: "assetGenericActive"))
+    }
+    
+    func unactive(
+        model:DPlan)
+    {
+        self.model = model
+        updateImage(image:#imageLiteral(resourceName: "assetGenericUnactive"))
+    }
 }
