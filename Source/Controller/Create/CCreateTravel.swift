6eaac5cb460eff96f5acfe2d3c2d7e060ae4ae18
vauxhall <zero.griffin@gmail.com>
2017-11-04 16:37:11 -0600

    Add initialiser to create-travel

--- Source/Controller/Create/CCreateTravel.swift
+++ Source/Controller/Create/CCreateTravel.swift
@@ -2,5 +2,17 @@ import Foundation
 
 final class CCreateTravel:Controller<ArchCreateTravel>
 {
+    init(
+        database:Database,
+        travel:DPlanTravel)
+    {
+        super.init()
+        model.database = database
+        model.travel = travel
+    }
     
+    required init?(coder:NSCoder)
+    {
+        return nil
+    }
 }
--- Source/Model/CreateTravel/MCreateTravel.swift
+++ Source/Model/CreateTravel/MCreateTravel.swift
@@ -2,5 +2,6 @@ import Foundation
 
 final class MCreateTravel:Model<ArchCreateTravel>
 {
-    
+    weak var database:Database?
+    weak var travel:DPlanTravel?
 }
