8c8ccf21fc1017d3f2072721bbf80b586ef98e17
vauxhall <zero.griffin@gmail.com>
2017-11-05 00:00:13 -0600

    Add hover to create-travel list

--- Source/View/CreateTravel/VCreateTravelListCell.swift
+++ Source/View/CreateTravel/VCreateTravelListCell.swift
@@ -2,11 +2,26 @@ import UIKit
 
 final class VCreateTravelListCell:UICollectionViewCell
 {
+    private weak var imageView:UIImageView!
+    
     override init(frame:CGRect)
     {
         super.init(frame:frame)
         clipsToBounds = true
         backgroundColor = UIColor.clear
+        
+        let imageView:UIImageView = UIImageView()
+        imageView.isUserInteractionEnabled = false
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.contentMode = UIViewContentMode.center
+        imageView.clipsToBounds = true
+        self.imageView = imageView
+        
+        addSubview(imageView)
+        
+        NSLayoutConstraint.equals(
+            view:imageView,
+            toView:self)
     }
     
     required init?(coder:NSCoder)
@@ -14,10 +29,42 @@ final class VCreateTravelListCell:UICollectionViewCell
         return nil
     }
     
+    override var isSelected:Bool
+    {
+        didSet
+        {
+            hover()
+        }
+    }
+    
+    override var isHighlighted:Bool
+    {
+        didSet
+        {
+            hover()
+        }
+    }
+    
+    //MARK: private
+    
+    private func hover()
+    {
+        if isSelected || isHighlighted
+        {
+            imageView.tintColor = UIColor.white
+        }
+        else
+        {
+            imageView.tintColor = UIColor.colourBackgroundDark
+        }
+    }
+    
     //MARK: internal
     
     func config(model:MCreateTravelProtocol)
     {
-        
+        imageView.image = model.icon.withRenderingMode(
+            UIImageRenderingMode.alwaysTemplate)
+        hover()
     }
 }
