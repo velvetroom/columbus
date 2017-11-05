a8060b33c6fa6c9b12b19ad6e8d246b2c6cae473
vauxhall <zero.griffin@gmail.com>
2017-11-04 23:34:48 -0600

    Factory index map for create-travel

--- Source/Model/CreateTravel/MCreateTravel.swift
+++ Source/Model/CreateTravel/MCreateTravel.swift
@@ -5,10 +5,13 @@ final class MCreateTravel:Model<ArchCreateTravel>
     weak var database:Database?
     weak var travel:DPlanTravel?
     let items:[MCreateTravelProtocol]
+    let indexMap:[DPlanTravelMode:Int]
     
     required init()
     {
         items = MCreateTravel.factoryItems()
+        indexMap = MCreateTravel.factoryIndexMap(
+            items:items)
         
         super.init()
     }
