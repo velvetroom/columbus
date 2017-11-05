b5a04cc156cbbf37ae9187d22957c4913d7ac7d5
vauxhall <zero.griffin@gmail.com>
2017-11-05 15:45:31 -0600

    Create store kit delegate extension

--- Source/Model/Store/MStoreKit.swift
+++ Source/Model/Store/MStoreKit.swift
@@ -9,26 +9,4 @@ final class MStoreKit:
 {
     weak var model:MStore?
 
-    //MARK: storekit
-    
-    func request(
-        _ request:SKRequest,
-        didFailWithError error:Error)
-    {
-        model?.statusError(error:error.localizedDescription)
-    }
-    
-    func paymentQueue(
-        _ queue:SKPaymentQueue,
-        updatedTransactions transactions:[SKPaymentTransaction])
-    {
-        
-    }
-    
-    func productsRequest(
-        _ request:SKProductsRequest,
-        didReceive response:SKProductsResponse)
-    {
-        
-    }
 }
