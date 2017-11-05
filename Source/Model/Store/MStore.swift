48a36229a0d845eb33ce4280dbb9c9aa61ee591c
vauxhall <zero.griffin@gmail.com>
2017-11-05 03:51:32 -0600

    Factory perks and perks map

--- Source/Model/Store/MStore.swift
+++ Source/Model/Store/MStore.swift
@@ -3,6 +3,8 @@ import Foundation
 final class MStore:Model<ArchStore>
 {
     var error:String?
+    let perks:[MStorePerkProtocol]
+    let perksMap:[String:MStorePerkProtocol]
     let modelKit:MStoreKit
     let priceFormatter:NumberFormatter
     
@@ -10,6 +12,9 @@ final class MStore:Model<ArchStore>
     {
         modelKit = MStoreKit()
         priceFormatter = MStore.factoryPriceFormatter()
+        perks = MStore.factoryPerks()
+        perksMap = MStore.factoryPerksMap(
+            perks:perks)
         
         super.init()
         modelKit.model = self
--- Source/Model/Store/MStoreFactory.swift
+++ Source/Model/Store/MStoreFactory.swift
@@ -11,4 +11,27 @@ extension MStore
         
         return formatter
     }
+    
+    class func factoryPerks() -> [MStorePerkProtocol]
+    {
+        let perkUnlimited:MStorePerkUnlimited = MStorePerkUnlimited()
+        
+        let perks:[MStorePerkProtocol] = [
+            perkUnlimited]
+        
+        return perks
+    }
+    
+    class func factoryPerksMap(
+        perks:[MStorePerkProtocol]) -> [String:MStorePerkProtocol]
+    {
+        var map:[String:MStorePerkProtocol] = [:]
+        
+        for perk:MStorePerkProtocol in perks
+        {
+            map[perk.purchaseId] = perk
+        }
+        
+        return map
+    }
 }
