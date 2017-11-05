21904ff2c15f3b15bfb81ea96bbd35634634cfad
vauxhall <zero.griffin@gmail.com>
2017-11-05 15:35:18 -0600

    Add icon to view store loading

--- Source/View/Store/VStoreStatusLoading.swift
+++ Source/View/Store/VStoreStatusLoading.swift
@@ -2,5 +2,28 @@ import UIKit
 
 final class VStoreStatusLoading:View<ArchStore>
 {
+    required init(controller:CStore)
+    {
+        super.init(controller:controller)
+        clipsToBounds = true
+        isUserInteractionEnabled = false
+        
+        let icon:UIImageView = UIImageView()
+        icon.isUserInteractionEnabled = false
+        icon.translatesAutoresizingMaskIntoConstraints = false
+        icon.clipsToBounds = true
+        icon.contentMode = UIViewContentMode.center
+        icon.image = #imageLiteral(resourceName: "assetMenuStore")
+        
+        addSubview(icon)
+        
+        NSLayoutConstraint.equals(
+            view:icon,
+            toView:self)
+    }
     
+    required init?(coder:NSCoder)
+    {
+        return nil
+    }
 }
