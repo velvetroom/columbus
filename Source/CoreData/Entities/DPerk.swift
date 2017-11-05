14778ffe45dfa5fe89914f1161ada0e7f20a8f43
vauxhall <zero.griffin@gmail.com>
2017-11-05 01:53:24 -0600

    Add computed property for perk type

--- Source/CoreData/Entities/DPerk.swift
+++ Source/CoreData/Entities/DPerk.swift
@@ -2,5 +2,26 @@ import Foundation
 
 extension DPerk
 {
-    
+    var type:DPerkType
+    {
+        get
+        {
+            guard
+            
+                let type:DPerkType = DPerkType(
+                    rawValue:rawType)
+            
+            else
+            {
+                return DPerkType.error
+            }
+            
+            return type
+        }
+        
+        set(newValue)
+        {
+            rawType = newValue.rawValue
+        }
+    }
 }
