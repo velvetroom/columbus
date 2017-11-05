52d9bb21687abc296386adbb4548171ed9eabf46
vauxhall <zero.griffin@gmail.com>
2017-11-04 23:31:24 -0600

    Factory create-travel items

--- Source/Model/CreateTravel/MCreateTravel.swift
+++ Source/Model/CreateTravel/MCreateTravel.swift
@@ -4,4 +4,12 @@ final class MCreateTravel:Model<ArchCreateTravel>
 {
     weak var database:Database?
     weak var travel:DPlanTravel?
+    let items:[MCreateTravelProtocol]
+    
+    required init()
+    {
+        items = MCreateTravel.factoryItems()
+        
+        super.init()
+    }
 }
--- Source/Model/CreateTravel/MCreateTravelFactory.swift
+++ Source/Model/CreateTravel/MCreateTravelFactory.swift
@@ -3,4 +3,20 @@ import Foundation
 extension MCreateTravel
 {
     //MARK: internal
+    
+    class func factoryItems() -> [MCreateTravelProtocol]
+    {
+        let itemWalking:MCreateTravelWalking = MCreateTravelWalking()
+        let itemCycling:MCreateTravelCycling = MCreateTravelCycling()
+        let itemTransit:MCreateTravelTransit = MCreateTravelTransit()
+        let itemDriving:MCreateTravelDriving = MCreateTravelDriving()
+        
+        let items:[MCreateTravelProtocol] = [
+            itemWalking,
+            itemCycling,
+            itemTransit,
+            itemDriving]
+        
+        return items
+    }
 }
