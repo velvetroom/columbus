105c001a929ac610f080ce82c9d6eab1c04a9987
vauxhall <zero.griffin@gmail.com>
2017-11-05 18:47:25 -0600

    Layout store list cells

--- Source/View/Store/VStoreStatusReadyList.swift
+++ Source/View/Store/VStoreStatusReadyList.swift
@@ -20,4 +20,50 @@ final class VStoreStatusReadyList:VCollection<
     {
         return nil
     }
+    
+    override func collectionView(
+        _ collectionView:UICollectionView,
+        layout collectionViewLayout:UICollectionViewLayout,
+        sizeForItemAt indexPath:IndexPath) -> CGSize
+    {
+        guard
+        
+            let cellSize:CGSize = self.cellSize
+        
+        else
+        {
+            let width:CGFloat = collectionView.bounds.width
+            let cellSize:CGSize = CGSize(
+                width:width,
+                height:kCellHeight)
+            self.cellSize = cellSize
+            
+            return cellSize
+        }
+        
+        return cellSize
+    }
+    
+    override func collectionView(
+        _ collectionView:UICollectionView,
+        numberOfItemsInSection section:Int) -> Int
+    {
+        let count:Int = controller.model.perks.count
+        
+        return count
+    }
+    
+    override func collectionView(
+        _ collectionView:UICollectionView,
+        shouldSelectItemAt indexPath:IndexPath) -> Bool
+    {
+        return false
+    }
+    
+    override func collectionView(
+        _ collectionView:UICollectionView,
+        shouldHighlightItemAt indexPath:IndexPath) -> Bool
+    {
+        return false
+    }
 }
--- Source/View/Store/VStoreStatusReadyListCell.swift
+++ Source/View/Store/VStoreStatusReadyListCell.swift
@@ -2,4 +2,48 @@ import UIKit
 
 class VStoreStatusReadyListCell:UICollectionViewCell
 {
+    private(set) weak var imageView:UIImageView!
+    private(set) weak var controller:CStore?
+    private let kImageHeight:CGFloat = 150
+    
+    override init(frame:CGRect)
+    {
+        super.init(frame:frame)
+        backgroundColor = UIColor.white
+        clipsToBounds = true
+        
+        let imageView:UIImageView = UIImageView()
+        imageView.isUserInteractionEnabled = false
+        imageView.clipsToBounds = true
+        imageView.contentMode = UIViewContentMode.center
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        self.imageView = imageView
+        
+        addSubview(imageView)
+        
+        NSLayoutConstraint.topToTop(
+            view:imageView,
+            toView:self)
+        NSLayoutConstraint.height(
+            view:imageView,
+            constant:kImageHeight)
+        NSLayoutConstraint.equalsHorizontal(
+            view:imageView,
+            toView:self)
+    }
+    
+    required init?(coder:NSCoder)
+    {
+        return nil
+    }
+    
+    //MARK: internal
+    
+    func config(
+        controller:CStore,
+        model:MStorePerkProtocol)
+    {
+        self.controller = controller
+        self.imageView.image = model.icon
+    }
 }
