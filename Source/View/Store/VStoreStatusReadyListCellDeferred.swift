a77fa8062bf99db75c870f4d253a12362d470f4c
vauxhall <zero.griffin@gmail.com>
2017-11-06 14:32:24 -0600

    Layout deferred store cell

--- Source/View/Store/VStoreStatusReadyListCellDeferred.swift
+++ Source/View/Store/VStoreStatusReadyListCellDeferred.swift
@@ -2,4 +2,39 @@ import UIKit
 
 final class VStoreStatusReadyListCellDeferred:VStoreStatusReadyListCellAvailable
 {
+    private let kLabelFontSize:CGFloat = 15
+    private let kLabelHeight:CGFloat = 80
+    
+    override init(frame:CGRect)
+    {
+        super.init(frame:frame)
+        isUserInteractionEnabled = false
+        
+        let label:UILabel = UILabel()
+        label.isUserInteractionEnabled = false
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textAlignment = NSTextAlignment.center
+        label.backgroundColor = UIColor.clear
+        label.font = UIFont.regular(size:kLabelFontSize)
+        label.textColor = UIColor(white:0, alpha:0.4)
+        label.text = String.localizedView(
+            key:"VStoreStatusReadyListCellDeferred_label")
+        
+        addSubview(label)
+        
+        NSLayoutConstraint.bottomToBottom(
+            view:label,
+            toView:self)
+        NSLayoutConstraint.height(
+            view:label,
+            constant:kLabelHeight)
+        NSLayoutConstraint.equalsHorizontal(
+            view:label,
+            toView:self)
+    }
+    
+    required init?(coder:NSCoder)
+    {
+        return nil
+    }
 }
