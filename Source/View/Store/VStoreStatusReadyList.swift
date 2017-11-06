33eca896b93f52a04e3a5f1b213f14bcb8bcc398
vauxhall <zero.griffin@gmail.com>
2017-11-05 18:37:52 -0600

    Config store list

--- Source/View/Store/VStoreStatusReadyList.swift
+++ Source/View/Store/VStoreStatusReadyList.swift
@@ -4,5 +4,20 @@ final class VStoreStatusReadyList:VCollection<
     ArchStore,
     VStoreStatusReadyListCell>
 {
+    let kInterItem:CGFloat = 1
+    let kInsetsTop:CGFloat = 64
+    let kCollectionBottom:CGFloat = 50
+    private var cellSize:CGSize?
+    private let kCellHeight:CGFloat = 300
     
+    required init(controller:CStore)
+    {
+        super.init(controller:controller)
+        config()
+    }
+    
+    required init?(coder:NSCoder)
+    {
+        return nil
+    }
 }
--- Source/View/Store/VStoreStatusReadyListConfig.swift
+++ Source/View/Store/VStoreStatusReadyListConfig.swift
@@ -4,5 +4,31 @@ extension VStoreStatusReadyList
 {
     //MARK: internal
     
-    
+    func config()
+    {
+        backgroundColor = UIColor.colourBackgroundGray
+        collectionView.alwaysBounceVertical = true
+        
+        registerCell(
+            cell:VStoreStatusReadyListCellNew.self)
+        registerCell(
+            cell:VStoreStatusReadyListCellDeferred.self)
+        registerCell(
+            cell:VStoreStatusReadyListCellPurchased.self)
+        registerCell(
+            cell:VStoreStatusReadyListCellPurchasing.self)
+        registerCell(
+            cell:VStoreStatusReadyListCellUnavailable.self)
+        
+        if let flow:VCollectionFlow = collectionView.collectionViewLayout as? VCollectionFlow
+        {
+            flow.minimumLineSpacing = kInterItem
+            flow.minimumInteritemSpacing = kInterItem
+            flow.sectionInset = UIEdgeInsets(
+                top:kInsetsTop,
+                left:0,
+                bottom:kCollectionBottom,
+                right:0)
+        }
+    }
 }
