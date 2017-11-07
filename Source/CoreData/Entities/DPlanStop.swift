cf4c5a4c824449322a4639236e73fcefecf56551
vauxhall <zero.griffin@gmail.com>
2017-11-07 17:50:17 -0600

    When inserting draggable should be true

--- Source/CoreData/Entities/DPlanStop.swift
+++ Source/CoreData/Entities/DPlanStop.swift
@@ -2,4 +2,13 @@ import Foundation
 
 extension DPlanStop
 {
+    public override func awakeFromInsert()
+    {
+        draggable = true
+    }
+    
+    public override func awakeFromFetch()
+    {
+        draggable = false
+    }
 }
