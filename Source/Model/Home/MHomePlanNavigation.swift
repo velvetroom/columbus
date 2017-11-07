a5f3cb5ccb00a9f64849524286ee4812ef2d7df5
vauxhall <zero.griffin@gmail.com>
2017-11-07 17:12:22 -0600

    Add stops on map

--- Source/Model/Home/MHomePlanNavigation.swift
+++ Source/Model/Home/MHomePlanNavigation.swift
@@ -4,9 +4,29 @@ extension MHomePlan
 {
     //MARK: private
     
+    private func addStops(
+        stops:[DPlanStop],
+        view:VHomeReady)
+    {
+        for stop:DPlanStop in stops
+        {
+            view.viewMap.addStop(stop:stop)
+        }
+    }
+    
     //MARK: internal
     
     func addMapNavigation(view:VHomeReady)
     {
+        guard
+        
+            let stops:[DPlanStop] = plan.stops?.array as? [DPlanStop]
+        
+        else
+        {
+            return
+        }
+        
+        addStops(stops:stops, view:view)
     }
 }
