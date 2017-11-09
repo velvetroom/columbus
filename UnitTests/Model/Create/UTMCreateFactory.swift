e5699a45be599f28b38bbceb99f2c5dcf824b599
vauxhall <zero.griffin@gmail.com>
2017-11-08 19:10:56 -0600

    Test factory settings

--- UnitTests/Model/Create/UTMCreateFactory.swift
+++ UnitTests/Model/Create/UTMCreateFactory.swift
@@ -1,6 +1,30 @@
 import XCTest
+@testable import columbus
 
-class UTMCreateFactory:XCTestCase
+final class UTMCreateFactory:XCTestCase
 {
+    private let kWait:TimeInterval = 1
     
+    //MARK: tests
+    
+    func testFactorySettings()
+    {
+        let fetchExpectation:XCTestExpectation = expectation(
+            description:"fetch expectation")
+        
+        let model:MCreate = MCreate()
+        model.factorySettings()
+        
+        waitForExpectations(timeout:kWait)
+        { (error:Error?) in
+            
+            XCTAssertNotNil(
+                model.database,
+                "failed loading database")
+            
+            XCTAssertNotNil(
+                model.settings,
+                "failed loading settings")
+        }
+    }
 }
