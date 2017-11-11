af3af825c96a18449a87fb9399234fed59265e88
vauxhall <zero.griffin@gmail.com>
2017-11-10 21:12:33 -0600

    Test model create load

---
+++ UnitTests/Model/Create/UTMCreateLoad.swift
@@ -0,0 +1,49 @@
+import XCTest
+@testable import columbus
+
+final class UTMCreateLoad:XCTestCase
+{
+    private var model:MCreate?
+    private let kWait:TimeInterval = 1
+    private let kAsyncAfter:TimeInterval = 0.5
+    
+    override func setUp()
+    {
+        super.setUp()
+        
+        model = MCreate()
+    }
+    
+    //MARK: internal
+    
+    func testLoadSettings()
+    {
+        let settingsExpectation:XCTestExpectation = expectation(
+            description:"settings expectation")
+        
+        let bundle:Bundle = Bundle(for:UTMCreateLoad.self)
+        model?.loadSettings(bundle:bundle)
+        
+        DispatchQueue.main.asyncAfter(
+            deadline:DispatchTime.now() + kAsyncAfter)
+        {
+            settingsExpectation.fulfill()
+        }
+        
+        waitForExpectations(timeout:kWait)
+        { [weak self] (error:Error?) in
+            
+            XCTAssertNotNil(
+                self?.model?.database,
+                "failed loading database")
+            
+            XCTAssertNotNil(
+                self?.model?.settings,
+                "failed loading settings")
+            
+            XCTAssertNotNil(
+                self?.model?.status,
+                "faliled creating status")
+        }
+    }
+}
