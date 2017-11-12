16b5cecf4cf1ecbbf459f49531d93763d5f8068d
vauxhall <zero.griffin@gmail.com>
2017-11-12 06:50:35 -0600

    Layout search list cell

--- Source/View/CreateSearch/VCreateSearchFactory.swift
+++ Source/View/CreateSearch/VCreateSearchFactory.swift
@@ -9,10 +9,25 @@ extension VCreateSearch
         let viewCancel:VCreateSearchCancel = VCreateSearchCancel(
             controller:controller)
         
+        let viewList:VCreateSearchList = VCreateSearchList(
+            controller:controller)
+        
         addSubview(viewCancel)
+        addSubview(viewList)
         
         NSLayoutConstraint.equals(
             view:viewCancel,
+            toView:self)
+        
+        NSLayoutConstraint.topToTop(
+            view:viewList,
+            toView:self,
+            constant:-viewList.kHeight)
+        NSLayoutConstraint.height(
+            view:viewList,
+            constant:viewList.kHeight)
+        NSLayoutConstraint.equalsHorizontal(
+            view:viewList,
             toView:self)
     }
 }
--- Source/View/CreateSearch/VCreateSearchList.swift
+++ Source/View/CreateSearch/VCreateSearchList.swift
@@ -1,6 +1,9 @@
 import UIKit
 
 final class VCreateSearchList:VCollection
+    <ArchCreateSearch,
+    VCreateSearchListCell>
 {
-    
+    weak var layoutTop:NSLayoutConstraint!
+    let kHeight:CGFloat = 300
 }
