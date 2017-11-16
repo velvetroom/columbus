2ae9d35a500087e49b88febd0332dc6282d0b97e
vauxhall <zero.griffin@gmail.com>
2017-11-15 22:15:09 -0600

    Create projects model

---
+++ Source/Model/SettingsMemory/MSettingsMemoryProjects.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+final class MSettingsMemoryProjects
+{
+    var map:[String:MSettingsMemoryProjects]
+    var size:CGFloat
+    
+    init()
+    {
+        map = [:]
+        size = 0
+    }
+}
