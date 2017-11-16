80b1c89bcf17f6a79134d30d0e0d1e9d061ea1bb
vauxhall <zero.griffin@gmail.com>
2017-11-15 19:00:54 -0600

    Config list view

--- Source/View/SettingsMemory/VSettingsMemoryList.swift
+++ Source/View/SettingsMemory/VSettingsMemoryList.swift
@@ -4,5 +4,18 @@ final class VSettingsMemoryList:VCollection<
     ArchSettingsMemory,
     VSettingsMemoryListCell>
 {
+    let kInterItem:CGFloat = 1
+    let kHeaderHeight:CGFloat = 124
+    let kCollectionBottom:CGFloat = 10
     
+    required init(controller:CSettingsMemory)
+    {
+        super.init(controller:controller)
+        config()
+    }
+    
+    required init?(coder:NSCoder)
+    {
+        return nil
+    }
 }
--- Source/View/SettingsMemory/VSettingsMemoryListConfig.swift
+++ Source/View/SettingsMemory/VSettingsMemoryListConfig.swift
@@ -4,5 +4,23 @@ extension VSettingsMemoryList
 {
     //MARK: internal
     
-    
+    func config()
+    {
+        backgroundColor = UIColor.colourBackgroundGray
+        collectionView.alwaysBounceVertical = true
+        
+        if let flow:VCollectionFlow = collectionView.collectionViewLayout as? VCollectionFlow
+        {
+            flow.minimumLineSpacing = kInterItem
+            flow.minimumInteritemSpacing = kInterItem
+            flow.headerReferenceSize = CGSize(
+                width:0,
+                height:kHeaderHeight)
+            flow.sectionInset = UIEdgeInsets(
+                top:0,
+                left:0,
+                bottom:kCollectionBottom,
+                right:0)
+        }
+    }
 }
