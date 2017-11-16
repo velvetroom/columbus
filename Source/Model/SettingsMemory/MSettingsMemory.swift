ac4946f2b1dbe7d00de9d118a3b943d0901e86c8
vauxhall <zero.griffin@gmail.com>
2017-11-15 20:30:38 -0600

    Add load extension to memory

--- Source/Model/SettingsMemory/MSettingsMemory.swift
+++ Source/Model/SettingsMemory/MSettingsMemory.swift
@@ -4,4 +4,11 @@ final class MSettingsMemory:Model<ArchSettingsMemory>
 {
     var database:Database?
     var settings:DSettings?
+    var items:[MSettingsMemoryItem]
+    
+    required init()
+    {
+        items = []
+        super.init()
+    }
 }
