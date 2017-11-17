7bef1eb5d050e90b1cb5dcc5893a616ca3d6afe8
vauxhall <zero.griffin@gmail.com>
2017-11-16 22:32:07 -0600

    Create controller parent transition struct

---
+++ Source/Controller/Abstract/ControllerParentTransition.swift
@@ -0,0 +1,18 @@
+import UIKit
+
+struct ControllerParentTransition
+{
+    struct Vertical
+    {
+        static let top:CGFloat = -1
+        static let bottom:CGFloat = 1
+        static let none:CGFloat = 0
+    }
+    
+    struct Horizontal
+    {
+        static let left:CGFloat = -1
+        static let right:CGFloat = 1
+        static let none:CGFloat = 0
+    }
+}
