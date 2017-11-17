456d6886ee888bde5023528c68fc44472d962305
vauxhall <zero.griffin@gmail.com>
2017-11-17 00:06:42 -0600

    Refactor model home

--- Source/Model/Home/MHomeInfoPlans.swift
+++ Source/Model/Home/MHomeInfoPlans.swift
@@ -3,8 +3,6 @@ import UIKit
 struct MHomeInfoPlans:MHomeInfoProtocol
 {
     let icon:UIImage = #imageLiteral(resourceName: "assetMenuPlans")
-    let title:String = String.localizedModel(
-        key:"MHomeInfoPlans_title")
-    let descr:String = String.localizedModel(
-        key:"MHomeInfoPlans_descr")
+    let title:String = String.localizedModel(key:"MHomeInfoPlans_title")
+    let descr:String = String.localizedModel(key:"MHomeInfoPlans_descr")
 }
