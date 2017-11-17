0c1d6a391c75da617fcb0c4692c0e3dc60f9726b
vauxhall <zero.griffin@gmail.com>
2017-11-16 22:36:14 -0600

    Remove transitions enum

--- Source/Controller/Abstract/ControllerParent+Transitions.swift
+++ Source/Controller/Abstract/ControllerParent+Transitions.swift
@@ -2,20 +2,6 @@ import UIKit
 
 extension ControllerParent
 {
-    enum Vertical:CGFloat
-    {
-        case top = -1
-        case bottom = 1
-        case none = 0
-    }
-    
-    enum Horizontal:CGFloat
-    {
-        case left = -1
-        case right = 1
-        case none = 0
-    }
-    
     //MARK: private
     
     private func slide(controller:UIViewController, left:CGFloat)
