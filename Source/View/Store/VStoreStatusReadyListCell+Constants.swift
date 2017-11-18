754ed34fac955a3694cbf1651e792bb922d25361
vauxhall <zero.griffin@gmail.com>
2017-11-17 21:29:57 -0600

    Factory constants extension

---
+++ Source/View/Store/VStoreStatusReadyListCell+Constants.swift
@@ -0,0 +1,15 @@
+import UIKit
+
+extension VStoreStatusReadyListCell
+{
+    struct Constants
+    {
+        static let imageHeight:CGFloat = 260
+        static let titleHeight:CGFloat = 90
+        static let titleFontSize:CGFloat = 18
+        static let descrMarginHorizontal:CGFloat = 20
+        static let descrTop:CGFloat = 10
+        static let descrMaxHeight:CGFloat = 300
+        static let descrFontSize:CGFloat = 14
+    }
+}
