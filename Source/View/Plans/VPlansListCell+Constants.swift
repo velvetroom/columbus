7dc6b91b73d7e688c13c236558c362f729919f3c
vauxhall <zero.griffin@gmail.com>
2017-11-18 02:14:32 -0600

    Create constants extension

---
+++ Source/View/Plans/VPlansListCell+Constants.swift
@@ -0,0 +1,19 @@
+import UIKit
+
+extension VPlansListCell
+{
+    struct Constants
+    {
+        static let routeWidth:CGFloat = 48
+        static let routeHeight:CGFloat = 66
+        static let labelOriginTop:CGFloat = 12
+        static let labelDestinationTop:CGFloat = 5
+        static let labelRouteHeight:CGFloat = 20
+        static let routeFontSize:CGFloat = 12
+        static let createdHeight:CGFloat = 42
+        static let createdWidth:CGFloat = 200
+        static let createdFontSize:CGFloat = 12
+        static let buttonMarginVertical:CGFloat = 20
+        static let buttonWidth:CGFloat = 70
+    }
+}
