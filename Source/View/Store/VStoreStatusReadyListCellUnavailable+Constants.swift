93fa39a01972601b4f6c827b3dd90d890c09748c
vauxhall <zero.griffin@gmail.com>
2017-11-17 21:39:37 -0600

    Create constants extension

---
+++ Source/View/Store/VStoreStatusReadyListCellUnavailable+Constants.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+extension VStoreStatusReadyListCellUnavailable
+{
+    struct Constants
+    {
+        static let labelFontSize:CGFloat = 16
+        static let labelHeight:CGFloat = 80
+    }
+}
