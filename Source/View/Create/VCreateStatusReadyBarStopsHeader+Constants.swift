b7a4c8f0e10335a2bdeb2f2e89c1571b828e7dcf
vauxhall <zero.griffin@gmail.com>
2017-11-18 01:00:29 -0600

    Create constants extension

---
+++ Source/View/Create/VCreateStatusReadyBarStopsHeader+Constants.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+extension VCreateStatusReadyBarStopsHeader
+{
+    struct Constants
+    {
+        static let iconTop:CGFloat = 70
+        static let iconHeight:CGFloat = 60
+        static let labelHeight:CGFloat = 60
+        static let fontSize:CGFloat = 15
+    }
+}
