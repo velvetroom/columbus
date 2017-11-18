23a73a8b228f7192b3c7eb9c4d92775e549a7178
vauxhall <zero.griffin@gmail.com>
2017-11-17 21:01:14 -0600

    Create map pin constants extension

---
+++ Source/View/Global/Map/VMapPin+Constants.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+extension VMapPin
+{
+    struct Constants
+    {
+        static let imageOffsetY:CGFloat = -17
+        static let imageDragginOffsetY:CGFloat = 24
+        static let animationDuration:TimeInterval = 0.3
+    }
+}
