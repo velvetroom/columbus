2b721df7bc59370132dec7a2f104cb57b51b764a
vauxhall <zero.griffin@gmail.com>
2017-11-17 22:56:58 -0600

    Create constants extension

---
+++ Source/View/Home/VHomeStandbyHeader+Constants.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+extension VHomeStandbyHeader
+{
+    struct Constants
+    {
+        static let fontSize:CGFloat = 14
+        static let iconTopRatio:CGFloat = 0.4
+        static let iconHeight:CGFloat = 80
+        static let titleTop:CGFloat = -10
+        static let titleHeight:CGFloat = 20
+    }
+}
