6cedfa8d295d974a4d6271c8d69ecb2854b76b76
vauxhall <zero.griffin@gmail.com>
2017-11-18 02:00:41 -0600

    Create constants extension

---
+++ Source/View/Plans/VPlansListHeader+Constants.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+extension VPlansListHeader
+{
+    struct Constants
+    {
+        static let textTop:CGFloat = 220
+        static let textHeight:CGFloat = 50
+        static let fontSize:CGFloat = 15
+        static let iconHeight:CGFloat = 120
+    }
+}
