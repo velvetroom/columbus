6e613e69b13e9d50ecc88b0dd26c549567ff9f25
vauxhall <zero.griffin@gmail.com>
2017-11-18 01:51:33 -0600

    Create constants extension

---
+++ Source/View/CreateSearch/VCreateSearchBaseListCell+Constants.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+extension VCreateSearchBaseListCell
+{
+    struct Constants
+    {
+        static let titleFontSize:CGFloat = 14
+        static let subtitleFontSize:CGFloat = 11
+        static let margin:CGFloat = 10
+        static let textMaxHeight:CGFloat = 1000
+    }
+}
