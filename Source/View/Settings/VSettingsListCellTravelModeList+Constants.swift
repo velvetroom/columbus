fec092416d1a6b314f86787648c3545e670e6358
vauxhall <zero.griffin@gmail.com>
2017-11-18 02:31:37 -0600

    Create constants extension

---
+++ Source/View/Settings/VSettingsListCellTravelModeList+Constants.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+extension VSettingsListCellTravelModeList
+{
+    struct Constants
+    {
+        static let selectorSize:CGFloat = 50
+        static let animationDuration:TimeInterval = 0.2
+        static let waitToSelect:TimeInterval = 0.1
+    }
+}
