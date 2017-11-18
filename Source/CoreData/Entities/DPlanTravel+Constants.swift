3874e6faeb8ea02cf98c5e3d8c629ef74d775cd2
vauxhall <zero.griffin@gmail.com>
2017-11-18 03:06:16 -0600

    Create constants extension

---
+++ Source/CoreData/Entities/DPlanTravel+Constants.swift
@@ -0,0 +1,11 @@
+import Foundation
+
+extension DPlanTravel
+{
+    struct Constants
+    {
+        static let distanceConversion:[DSettingsDistance:Float] = [
+            DSettingsDistance.kilometres : 1000.0,
+            DSettingsDistance.miles : 1609.34449789256338]
+    }
+}
