b88f50d4d567743958faf664736eba093fafa580
vauxhall <zero.griffin@gmail.com>
2017-11-18 02:36:56 -0600

    Create constants extension

---
+++ Source/View/Settings/VSettingsListCellDetailLevel+Constants.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+extension VSettingsListCellDetailLevel
+{
+    struct Constants
+    {
+        static let captionHeight:CGFloat = 90
+        static let captionLeft:CGFloat = 20
+        static let titleFontSize:CGFloat = 16
+        static let descrFontSize:CGFloat = 14
+        static let listTop:CGFloat = 100
+        static let listBottom:CGFloat = -20
+    }
+}
