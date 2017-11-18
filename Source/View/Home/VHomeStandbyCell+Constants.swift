3305b62a3fb3076b43e459c6532bd597a8b5a2bd
vauxhall <zero.griffin@gmail.com>
2017-11-17 22:59:37 -0600

    Create constants extension

---
+++ Source/View/Home/VHomeStandbyCell+Constants.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+extension VHomeStandbyCell
+{
+    struct Constants
+    {
+        static let iconSize:CGFloat = 60
+        static let infoTop:CGFloat = 16
+        static let infoRight:CGFloat = -20
+        static let infoMaxHeight:CGFloat = 600
+        static let titleFontSize:CGFloat = 15
+        static let descrFontSize:CGFloat = 14
+    }
+}
