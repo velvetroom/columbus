ff08a469327a28de07f500c5a6427d632ff50dcc
vauxhall <zero.griffin@gmail.com>
2017-11-18 00:44:34 -0600

    Create constants extension

---
+++ Source/View/Create/VCreateStatusReady+Constants.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+extension VCreateStatusReady
+{
+    struct Constants
+    {
+        static let bottom:CGFloat = -50
+        static let mapMenuWidth:CGFloat = 70
+        static let waitForAnimation:TimeInterval = 1.4
+    }
+}
