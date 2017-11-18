c7250a5b0bfdb21ce3a6e4512c590c4c60acb369
vauxhall <zero.griffin@gmail.com>
2017-11-18 02:40:00 -0600

    Create constants extension

---
+++ Source/View/Settings/VSettingsListCellDetailLevelList+Constants.swift
@@ -0,0 +1,17 @@
+import UIKit
+
+extension VSettingsListCellDetailLevelList
+{
+    struct Constants
+    {
+        static let selectorLeft:CGFloat = 20
+        static let selectorHeight:CGFloat = 30
+        static let railWidth:CGFloat = 2
+        static let railLeft:CGFloat = 30
+        static let animationDuration:TimeInterval = 0.2
+        static let waitToSelect:TimeInterval = 0.1
+        static let cellWidth:CGFloat = 140
+        static let labelRight:CGFloat = -10
+        static let fontSize:CGFloat = 14
+    }
+}
