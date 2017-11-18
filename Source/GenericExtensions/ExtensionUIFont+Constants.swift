accc3c3df1f21248b5cb17be245bba63ef79172f
vauxhall <zero.griffin@gmail.com>
2017-11-18 03:16:41 -0600

    Create constants extension

---
+++ Source/GenericExtensions/ExtensionUIFont+Constants.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+extension UIFont
+{
+    struct Constants
+    {
+        static let fontLight:String = "HelveticaNeue-Thin"
+        static let fontRegular:String = "HelveticaNeue"
+        static let fontMedium:String = "HelveticaNeue-Medium"
+        static let fontBold:String = "HelveticaNeue-Bold"
+    }
+}
