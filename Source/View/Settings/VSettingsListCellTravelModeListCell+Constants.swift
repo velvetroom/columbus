a0f75e3451b562a991959b081099747ac349f5d1
vauxhall <zero.griffin@gmail.com>
2017-11-18 02:34:44 -0600

    Create constants extension

---
+++ Source/View/Settings/VSettingsListCellTravelModeListCell+Constants.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+extension VSettingsListCellTravelModeListCell
+{
+    struct Constants
+    {
+        static let titleFontSize:CGFloat = 12
+        static let titleHeight:CGFloat = 30
+    }
+}
