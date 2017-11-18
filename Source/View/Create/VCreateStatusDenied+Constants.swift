ed572d5bee25af749293fe9359eac4099b2cc6f1
vauxhall <zero.griffin@gmail.com>
2017-11-18 00:39:39 -0600

    Create constants extension

---
+++ Source/View/Create/VCreateStatusDenied+Constants.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+extension VCreateStatusDenied
+{
+    struct Constants
+    {
+        static let imageTop:CGFloat = 210
+        static let imageHeight:CGFloat = 50
+        static let labelHeight:CGFloat = 150
+        static let fontSizeTitle:CGFloat = 18
+        static let fontSizeDescr:CGFloat = 15
+    }
+}
