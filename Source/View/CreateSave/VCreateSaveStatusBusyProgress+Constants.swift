b930d3fbaec5776d1be628182056f00ce6cc8d1a
vauxhall <zero.griffin@gmail.com>
2017-11-18 01:34:48 -0600

    Create constants extension

---
+++ Source/View/CreateSave/VCreateSaveStatusBusyProgress+Constants.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+extension VCreateSaveStatusBusyProgress
+{
+    struct Constants
+    {
+        static let barAlpha:CGFloat = 0.3
+        static let backgroundAlpha:CGFloat = 0.1
+    }
+}
