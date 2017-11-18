a1b7eeb9509a83b3527493d8b08bfe6f293471fe
vauxhall <zero.griffin@gmail.com>
2017-11-18 01:26:31 -0600

    Refactor views

--- Source/View/Create/VCreateStatusReadyBarTravelCell+Factory.swift
+++ Source/View/Create/VCreateStatusReadyBarTravelCell+Factory.swift
@@ -2,29 +2,20 @@ import UIKit
 
 extension VCreateStatusReadyBarTravelCell
 {
-    private static let kImageMap:[
-        DPlanTravelMode:UIImage] = [
-            DPlanTravelMode.walking:#imageLiteral(resourceName: "assetMapWalking"),
-            DPlanTravelMode.cycling:#imageLiteral(resourceName: "assetMapCycling"),
-            DPlanTravelMode.transit:#imageLiteral(resourceName: "assetMapTransit"),
-            DPlanTravelMode.driving:#imageLiteral(resourceName: "assetMapDriving")]
-    
     //MARK: internal
     
-    class func factoryImage(
-        mode:DPlanTravelMode) -> UIImage?
+    class func factoryImage(mode:DPlanTravelMode) -> UIImage?
     {
         guard
         
-            let image:UIImage = kImageMap[mode]
+            let image:UIImage = VCreateStatusReadyBarTravelCell.Constants.imageMap[mode]
         
         else
         {
             return nil
         }
         
-        return image.withRenderingMode(
-            UIImageRenderingMode.alwaysTemplate)
+        return image.withRenderingMode(UIImageRenderingMode.alwaysTemplate)
     }
     
     func factoryViews()
