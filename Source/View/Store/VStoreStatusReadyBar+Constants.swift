6f2aa73e5bdd7f726ed4484b8115acee541f42f8
vauxhall <zero.griffin@gmail.com>
2017-11-17 21:16:50 -0600

    Create constants extension

---
+++ Source/View/Store/VStoreStatusReadyBar+Constants.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+extension VStoreStatusReadyBar
+{
+    struct Constants
+    {
+        static let titleLeft:CGFloat = 20
+        static let titleWidth:CGFloat = 150
+        static let borderHeight:CGFloat = 1
+        static let fontSize:CGFloat = 14
+        static let restoreWidth:CGFloat = 156
+    }
+}
