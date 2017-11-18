cc4948e028858a0fbcbcd38179327c815aef7432
vauxhall <zero.griffin@gmail.com>
2017-11-18 01:27:32 -0600

    Create constants extension

---
+++ Source/View/Create/VCreateStatusReadyBarLoader+Constants.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+extension VCreateStatusReadyBarLoader
+{
+    struct Constants
+    {
+        static let animationDuration:TimeInterval = 1
+        static let fadeDuration:TimeInterval = 0.3
+    }
+}
