82c3f9004655e60290c81f2f8a81cc4f7ad5ffe2
vauxhall <zero.griffin@gmail.com>
2017-11-18 03:20:54 -0600

    Create constants extension

---
+++ Source/GenericExtensions/ExtensionString+Constants.swift
@@ -0,0 +1,11 @@
+import Foundation
+
+extension String
+{
+    struct Constants
+    {
+        static let tableController:String = "LocalizableController"
+        static let tableModel:String = "LocalizableModel"
+        static let tableView:String = "LocalizableView"
+    }
+}
