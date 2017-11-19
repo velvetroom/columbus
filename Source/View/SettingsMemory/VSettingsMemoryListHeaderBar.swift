a5102aac9307bc174e6fee4e6ffd613934146ec2
vauxhall <zero.griffin@gmail.com>
2017-11-19 12:31:08 -0600

    Layout border in line

--- Source/View/SettingsMemory/VSettingsMemoryListHeaderBar.swift
+++ Source/View/SettingsMemory/VSettingsMemoryListHeaderBar.swift
@@ -2,5 +2,16 @@ import UIKit
 
 final class VSettingsMemoryListHeaderBar:View<ArchSettingsMemory>
 {
+    required init(controller:CSettingsMemory)
+    {
+        super.init(controller:controller)
+        isUserInteractionEnabled = false
+        backgroundColor = UIColor.colourBackgroundGray
+        layer.cornerRadius = VSettingsMemoryListHeaderBar.Constants.cornerRadius
+    }
     
+    required init?(coder:NSCoder)
+    {
+        return nil
+    }
 }
--- Source/View/SettingsMemory/VSettingsMemoryListHeaderBarLine.swift
+++ Source/View/SettingsMemory/VSettingsMemoryListHeaderBarLine.swift
@@ -6,6 +6,20 @@ final class VSettingsMemoryListHeaderBarLine:View<ArchSettingsMemory>
     {
         super.init(controller:controller)
         isUserInteractionEnabled = false
+        
+        let border:VBorder = VBorder(colour:UIColor.white)
+        
+        addSubview(border)
+        
+        NSLayoutConstraint.equalsVertical(
+            view:border,
+            toView:self)
+        NSLayoutConstraint.rightToRight(
+            view:border,
+            toView:self)
+        NSLayoutConstraint.width(
+            view:border,
+            constant:ViewMain.Constants.borderWidth)
     }
     
     required init?(coder:NSCoder)
