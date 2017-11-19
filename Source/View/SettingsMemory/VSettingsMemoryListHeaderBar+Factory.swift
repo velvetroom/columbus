7e7e6d62db46531a0061b0686c253e39a7e9fddb
vauxhall <zero.griffin@gmail.com>
2017-11-19 12:40:23 -0600

    Layout line views

--- Source/View/SettingsMemory/VSettingsMemoryListHeaderBar+Factory.swift
+++ Source/View/SettingsMemory/VSettingsMemoryListHeaderBar+Factory.swift
@@ -3,4 +3,36 @@ import UIKit
 extension VSettingsMemoryListHeaderBar
 {
     //MARK: internal
+    
+    func factoryViews()
+    {
+        let lineColumbus:VSettingsMemoryListHeaderBarLine = VSettingsMemoryListHeaderBarLine(
+            controller:controller)
+        lineColumbus.backgroundColor = UIColor.colourSuccess
+        
+        let lineOther:VSettingsMemoryListHeaderBarLine = VSettingsMemoryListHeaderBarLine(
+            controller:controller)
+        lineOther.backgroundColor = UIColor(white:0, alpha:0.2)
+        
+        addSubview(lineColumbus)
+        addSubview(lineOther)
+        
+        NSLayoutConstraint.equalsVertical(
+            view:lineColumbus,
+            toView:self)
+        NSLayoutConstraint.leftToLeft(
+            view:lineColumbus,
+            toView:self)
+        layoutLineColumbusWidth = NSLayoutConstraint.width(
+            view:lineColumbus)
+        
+        NSLayoutConstraint.equalsVertical(
+            view:lineOther,
+            toView:self)
+        NSLayoutConstraint.leftToRight(
+            view:lineOther,
+            toView:lineColumbus)
+        layoutLineOtherWidth = NSLayoutConstraint.width(
+            view:lineOther)
+    }
 }
--- Source/View/SettingsMemory/VSettingsMemoryListHeaderBar.swift
+++ Source/View/SettingsMemory/VSettingsMemoryListHeaderBar.swift
@@ -2,12 +2,17 @@ import UIKit
 
 final class VSettingsMemoryListHeaderBar:View<ArchSettingsMemory>
 {
+    weak var layoutLineColumbusWidth:NSLayoutConstraint!
+    weak var layoutLineOtherWidth:NSLayoutConstraint!
+    
     required init(controller:CSettingsMemory)
     {
         super.init(controller:controller)
         isUserInteractionEnabled = false
         backgroundColor = UIColor.colourBackgroundGray
         layer.cornerRadius = VSettingsMemoryListHeaderBar.Constants.cornerRadius
+        
+        factoryViews()
     }
     
     required init?(coder:NSCoder)
