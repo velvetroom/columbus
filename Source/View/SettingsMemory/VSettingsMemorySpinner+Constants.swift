038c5dd2c8c297a86fcdacfc0f52c5fc069d3668
vauxhall <zero.griffin@gmail.com>
2017-11-19 02:09:03 -0600

    Create constants extension

---
+++ Source/View/SettingsMemory/VSettingsMemorySpinner+Constants.swift
@@ -0,0 +1,25 @@
+import UIKit
+
+extension VSettingsMemorySpinner
+{
+    struct Constants
+    {
+        static let images:[UIImage] = [
+            #imageLiteral(resourceName: "assetMeassuring0"),
+            #imageLiteral(resourceName: "assetMeassuring1"),
+            #imageLiteral(resourceName: "assetMeassuring2"),
+            #imageLiteral(resourceName: "assetMeassuring3"),
+            #imageLiteral(resourceName: "assetMeassuring4"),
+            #imageLiteral(resourceName: "assetMeassuring5"),
+            #imageLiteral(resourceName: "assetMeassuring6"),
+            #imageLiteral(resourceName: "assetMeassuring7"),
+            #imageLiteral(resourceName: "assetMeassuring8"),
+            #imageLiteral(resourceName: "assetMeassuring9"),
+            #imageLiteral(resourceName: "assetMeassuring9"),
+            #imageLiteral(resourceName: "assetMeassuring10"),
+            #imageLiteral(resourceName: "assetMeassuring11"),
+            #imageLiteral(resourceName: "assetMeassuring12")]
+        
+        static let animationDuration:TimeInterval = 2
+    }
+}
