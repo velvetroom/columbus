45618281fabcf8080046e400b5445c66aee0139a
vauxhall <zero.griffin@gmail.com>
2017-11-19 00:29:08 -0600

    Add origin and destination properties

--- Source/CoreData/Entities/DPlan+Computed.swift
+++ Source/CoreData/Entities/DPlan+Computed.swift
@@ -2,5 +2,23 @@ import Foundation
 
 extension DPlan
 {
+    var origin:DPlanStop?
+    {
+        get
+        {
+            let stop:DPlanStop? = stops?.firstObject as? DPlanStop
+            
+            return stop
+        }
+    }
     
+    var destination:DPlanStop?
+    {
+        get
+        {
+            let stop:DPlanStop? = stops?.lastObject as? DPlanStop
+            
+            return stop
+        }
+    }
 }
