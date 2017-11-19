8fd3759975353b56f2ffd3665bcfd42637b06910
vauxhall <zero.griffin@gmail.com>
2017-11-19 03:18:49 -0600

    Create constants extension

---
+++ Source/View/SettingsMemory/VSettingsMemoryList+Constants.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+extension VSettingsMemoryList
+{
+    struct Constants
+    {
+        static let headerHeight:CGFloat = 200
+        static let footerHeight:CGFloat = 100
+    }
+}
