ce474dce6a30b13ba46d841c94d6946f0945f120
vauxhall <zero.griffin@gmail.com>
2017-11-19 12:44:25 -0600

    Create constants extension

---
+++ Source/View/SettingsMemory/VSettingsMemoryListHeader+Constants.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+extension VSettingsMemoryListHeader
+{
+    struct Constants
+    {
+        static let paddingHorizontal:CGFloat = 10
+        static let headingHeight:CGFloat = 40
+        static let barHeight:CGFloat = 30
+    }
+}
