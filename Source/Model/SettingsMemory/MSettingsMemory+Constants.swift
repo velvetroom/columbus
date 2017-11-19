f02976472944f920f3b4cd3161c0abdd9c876b69
vauxhall <zero.griffin@gmail.com>
2017-11-19 13:18:24 -0600

    Create constants extension

---
+++ Source/Model/SettingsMemory/MSettingsMemory+Constants.swift
@@ -0,0 +1,13 @@
+import Foundation
+
+extension MSettingsMemory
+{
+    struct Constants
+    {
+        struct Formatter
+        {
+            static let minIntegers:Int = 1
+            static let maxDecimals:Int = 1
+        }
+    }
+}
