ab82d54aec50bd0685fc9f3ddc99cfaa0f5be8e3
vauxhall <zero.griffin@gmail.com>
2017-11-19 18:59:50 -0600

    Create constants extension

--- Source/View/SettingsMemory/VSettingsMemoryListHeaderList.swift
+++ Source/View/SettingsMemory/VSettingsMemoryListHeaderList.swift
@@ -2,5 +2,13 @@ import UIKit
 
 final class VSettingsMemoryListHeaderList:View<ArchSettingsMemory>
 {
+    required init(controller:CSettingsMemory)
+    {
+        super.init(controller:controller)
+    }
     
+    required init?(coder:NSCoder)
+    {
+        return nil
+    }
 }
---
+++ Source/View/SettingsMemory/VSettingsMemoryListHeaderListBadge+Constants.swift
@@ -0,0 +1,9 @@
+import UIKit
+
+extension VSettingsMemoryListHeaderListBadge
+{
+    struct Constants
+    {
+        static let cornerRadius:CGFloat = 3
+    }
+}
