991c023669395b119e9cb1af1d8f56b174c9eb8b
vauxhall <zero.griffin@gmail.com>
2017-11-19 19:53:44 -0600

    Factory views in footer

--- Source/View/SettingsMemory/VSettingsMemoryListFooter+Factory.swift
+++ Source/View/SettingsMemory/VSettingsMemoryListFooter+Factory.swift
@@ -6,6 +6,39 @@ extension VSettingsMemoryListFooter
     
     func factoryViews()
     {
+        let labelTitle:UILabel = UILabel()
+        labelTitle.isUserInteractionEnabled = false
+        labelTitle.backgroundColor = UIColor.clear
+        labelTitle.translatesAutoresizingMaskIntoConstraints = false
+        labelTitle.font = UIFont.regular(size:VSettingsMemoryListFooter.Constants.titleFontSize)
+        labelTitle.textColor = UIColor.colourBackgroundDark
+        labelTitle.text = String.localizedView(key:"VSettingsMemoryListFooter_labelTitle")
         
+        let border:VBorder = VBorder(colour:UIColor.colourBackgroundGray)
+        
+        addSubview(labelTitle)
+        addSubview(border)
+        
+        NSLayoutConstraint.topToTop(
+            view:labelTitle,
+            toView:self)
+        NSLayoutConstraint.height(
+            view:labelTitle,
+            constant:VSettingsMemoryListFooter.Constants.titleHeight)
+        NSLayoutConstraint.leftToLeft(
+            view:labelTitle,
+            toView:self,
+            constant:VSettingsMemoryListFooter.Constants.paddingHorizontal)
+        
+        NSLayoutConstraint.topToBottom(
+            view:border,
+            toView:labelTitle)
+        NSLayoutConstraint.height(
+            view:border,
+            constant:ViewMain.Constants.borderWidth)
+        NSLayoutConstraint.equalsHorizontal(
+            view:border,
+            toView:self,
+            margin:VSettingsMemoryListFooter.Constants.paddingHorizontal)
     }
 }
--- Source/View/SettingsMemory/VSettingsMemoryListFooter.swift
+++ Source/View/SettingsMemory/VSettingsMemoryListFooter.swift
@@ -7,6 +7,8 @@ final class VSettingsMemoryListFooter:UICollectionReusableView
         super.init(frame:frame)
         clipsToBounds = true
         backgroundColor = UIColor.white
+        
+        factoryViews()
     }
     
     required init?(coder:NSCoder)
