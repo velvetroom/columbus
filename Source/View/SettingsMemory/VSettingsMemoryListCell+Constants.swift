0fe0d82dd2865ee82bfbf52c114c237e794f42bf
vauxhall <zero.griffin@gmail.com>
2017-11-21 09:51:43 -0600

    Create constants extension

---
+++ Source/View/SettingsMemory/VSettingsMemoryListCell+Constants.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+extension VSettingsMemoryListCell
+{
+    struct Constants
+    {
+        static let fontSize:CGFloat = 14
+        static let locationsHeight:CGFloat = 30
+        static let removeWidth:CGFloat = 100
+        static let removeLeft:CGFloat = 10
+    }
+}
