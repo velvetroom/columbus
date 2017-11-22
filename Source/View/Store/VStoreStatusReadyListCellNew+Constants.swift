5b1eb7040a9d6541777b18ac0db28f5ebd1a9eba
vauxhall <zero.griffin@gmail.com>
2017-11-21 23:08:13 -0600

    Add factory extension for store new cell

--- Source/View/Store/VStoreStatusReadyListCellNew+Constants.swift
+++ Source/View/Store/VStoreStatusReadyListCellNew+Constants.swift
@@ -4,8 +4,8 @@ extension VStoreStatusReadyListCellNew
 {
     struct Constants
     {
-        static let buttonFontSize:CGFloat = 16
+        static let buttonFontSize:CGFloat = 14
         static let buttonHeight:CGFloat = 50
-        static let buttonWidth:CGFloat = 110
+        static let buttonWidth:CGFloat = 100
     }
 }
--- Source/View/Store/VStoreStatusReadyListCellNew.swift
+++ Source/View/Store/VStoreStatusReadyListCellNew.swift
@@ -9,70 +9,7 @@ final class VStoreStatusReadyListCellNew:VStoreStatusReadyListCellAvailable
     {
         super.init(frame:frame)
         
-        let buttonPurchase:UIButton = UIButton()
-        buttonPurchase.translatesAutoresizingMaskIntoConstraints = false
-        buttonPurchase.setTitleColor(
-            UIColor.colourBackgroundDark,
-            for:UIControlState.normal)
-        buttonPurchase.setTitleColor(
-            UIColor.colourSuccess,
-            for:UIControlState.highlighted)
-        buttonPurchase.titleLabel!.font = UIFont.bold(
-            size:VStoreStatusReadyListCellNew.Constants.buttonFontSize)
-        buttonPurchase.setTitle(
-            String.localizedView(key:"VStoreStatusReadyListCellNew_buttonPurchase"),
-            for:UIControlState.normal)
-        buttonPurchase.addTarget(
-            self,
-            action:#selector(selectorPurchase(sender:)),
-            for:UIControlEvents.touchUpInside)
-        
-        let buttonRestore:UIButton = UIButton()
-        buttonRestore.translatesAutoresizingMaskIntoConstraints = false
-        buttonRestore.setTitleColor(
-            UIColor.colourFail,
-            for:UIControlState.normal)
-        buttonRestore.setTitleColor(
-            UIColor.colourBackgroundDark,
-            for:UIControlState.highlighted)
-        buttonRestore.titleLabel!.font = UIFont.bold(
-            size:VStoreStatusReadyListCellNew.Constants.buttonFontSize)
-        buttonRestore.setTitle(
-            String.localizedView(key:"VStoreStatusReadyListCellNew_buttonRestore"),
-            for:UIControlState.normal)
-        buttonRestore.addTarget(
-            self,
-            action:#selector(selectorRestore(sender:)),
-            for:UIControlEvents.touchUpInside)
-        
-        addSubview(buttonPurchase)
-        addSubview(buttonRestore)
-        
-        NSLayoutConstraint.bottomToBottom(
-            view:buttonRestore,
-            toView:self)
-        NSLayoutConstraint.height(
-            view:buttonRestore,
-            constant:VStoreStatusReadyListCellNew.Constants.buttonHeight)
-        NSLayoutConstraint.width(
-            view:buttonRestore,
-            constant:VStoreStatusReadyListCellNew.Constants.buttonWidth)
-        NSLayoutConstraint.leftToLeft(
-            view:buttonRestore,
-            toView:self)
-        
-        NSLayoutConstraint.bottomToBottom(
-            view:buttonPurchase,
-            toView:self)
-        NSLayoutConstraint.height(
-            view:buttonPurchase,
-            constant:VStoreStatusReadyListCellNew.Constants.buttonHeight)
-        NSLayoutConstraint.width(
-            view:buttonPurchase,
-            constant:VStoreStatusReadyListCellNew.Constants.buttonWidth)
-        NSLayoutConstraint.rightToRight(
-            view:buttonPurchase,
-            toView:self)
+        factoryViewsCellNew()
     }
     
     required init?(coder:NSCoder)
@@ -103,7 +40,7 @@ final class VStoreStatusReadyListCellNew:VStoreStatusReadyListCellAvailable
     //MARK: selectors
     
     @objc
-    private func selectorPurchase(sender button:UIButton)
+    func selectorPurchase(sender button:UIButton)
     {
         guard
             
@@ -118,7 +55,7 @@ final class VStoreStatusReadyListCellNew:VStoreStatusReadyListCellAvailable
     }
     
     @objc
-    private func selectorRestore(sender button:UIButton)
+    func selectorRestore(sender button:UIButton)
     {
         controller?.model.modelKit.restorePurchases()
     }
