dfa6a4a74b1c98b37ef92243d586539ae9277707
vauxhall <zero.griffin@gmail.com>
2017-11-23 15:35:23 -0600

    Update storage layout

--- Source/View/Settings/VSettingsListCellStorage+Constants.swift
+++ Source/View/Settings/VSettingsListCellStorage+Constants.swift
@@ -4,6 +4,8 @@ extension VSettingsListCellStorage
 {
     struct Constants
     {
-        static let titleFontSize:CGFloat = 16
+        static let titleFontSize:CGFloat = 14
+        static let iconWidth:CGFloat = 60
+        static let alphaHighlighted:CGFloat = 0.3
     }
 }
--- Source/View/Settings/VSettingsListCellStorage.swift
+++ Source/View/Settings/VSettingsListCellStorage.swift
@@ -8,30 +8,63 @@ final class VSettingsListCellStorage:VSettingsListCell
     {
         super.init(frame:frame)
         
+        let icon:UIImageView = UIImageView()
+        icon.isUserInteractionEnabled = false
+        icon.translatesAutoresizingMaskIntoConstraints = false
+        icon.clipsToBounds = true
+        icon.contentMode = UIViewContentMode.center
+        icon.image = #imageLiteral(resourceName: "assetGenericStorage")
+        
+        let label:UILabel = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.backgroundColor = UIColor.clear
+        label.isUserInteractionEnabled = false
+        label.text = String.localizedView(key:"VSettingsListCellStorage_title")
+        label.font = UIFont.medium(size:VSettingsListCellStorage.Constants.titleFontSize)
+        label.textColor = UIColor.colourBackgroundDark
+        
         let button:UIButton = UIButton()
         button.clipsToBounds = true
         button.translatesAutoresizingMaskIntoConstraints = false
-        button.setTitleColor(
-            UIColor.colourBackgroundDark,
-            for:UIControlState.normal)
-        button.setTitleColor(
-            UIColor.colourBackgroundGray,
-            for:UIControlState.highlighted)
-        button.setTitle(
-            String.localizedView(key:"VSettingsListCellStorage_title"),
-            for:UIControlState.normal)
-        button.titleLabel!.font = UIFont.regular(
-            size:VSettingsListCellStorage.Constants.titleFontSize)
+        button.addTarget(
+            self,
+            action:#selector(selectorHighlighted(sender:)),
+            for:UIControlEvents.touchDown)
+        button.addTarget(
+            self,
+            action:#selector(selectorUnhighlighted(sender:)),
+            for:UIControlEvents.touchUpOutside)
         button.addTarget(
             self,
             action:#selector(selectorButton(sender:)),
             for:UIControlEvents.touchUpInside)
         
+        addSubview(icon)
+        addSubview(label)
         addSubview(button)
+        
+        NSLayoutConstraint.equalsVertical(
+            view:icon,
+            toView:self)
+        NSLayoutConstraint.leftToLeft(
+            view:icon,
+            toView:self)
+        NSLayoutConstraint.width(
+            view:icon,
+            constant:VSettingsListCellStorage.Constants.iconWidth)
         
         NSLayoutConstraint.equals(
             view:button,
             toView:self)
+        
+        NSLayoutConstraint.equalsVertical(
+            view:label,
+            toView:self)
+        NSLayoutConstraint.leftToRight(
+            view:label,
+            toView:icon)
+        NSLayoutConstraint.widthGreaterOrEqual(
+            view:label)
     }
     
     required init?(coder:NSCoder)
@@ -62,8 +95,22 @@ final class VSettingsListCellStorage:VSettingsListCell
     //MARK: selectors
     
     @objc
+    private func selectorHighlighted(sender button:UIButton)
+    {
+        alpha = VSettingsListCellStorage.Constants.alphaHighlighted
+    }
+    
+    @objc
+    private func selectorUnhighlighted(sender button:UIButton)
+    {
+        alpha = 1
+    }
+    
+    @objc
     private func selectorButton(sender button:UIButton)
     {
+        selectorUnhighlighted(sender:button)
+        
         guard
         
             let database:Database = model?.database,
