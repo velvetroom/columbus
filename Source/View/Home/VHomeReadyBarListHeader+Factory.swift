b268855558095115126f601e3d686ffe3d17873e
vauxhall <zero.griffin@gmail.com>
2017-11-24 20:18:01 -0600

    Layout subviews in header

--- Source/View/Home/VHomeReadyBarListHeader+Factory.swift
+++ Source/View/Home/VHomeReadyBarListHeader+Factory.swift
@@ -7,24 +7,46 @@ extension VHomeReadyBarListHeader
     func factoryViews()
     {
         let border:VBorder = VBorder(colour:UIColor(white:0, alpha:0.2))
+      
+        let colour:UIColor = UIColor.colourBackgroundDark.withAlphaComponent(0.5)
+        
+        let iconDistance:UIImageView = UIImageView()
+        iconDistance.translatesAutoresizingMaskIntoConstraints = false
+        iconDistance.clipsToBounds = true
+        iconDistance.contentMode = UIViewContentMode.center
+        iconDistance.isUserInteractionEnabled = false
+        iconDistance.image = #imageLiteral(resourceName: "assetMapDistance").withRenderingMode(UIImageRenderingMode.alwaysTemplate)
+        iconDistance.tintColor = colour
+        
+        let iconDuration:UIImageView = UIImageView()
+        iconDuration.translatesAutoresizingMaskIntoConstraints = false
+        iconDuration.clipsToBounds = true
+        iconDuration.isUserInteractionEnabled = false
+        iconDuration.contentMode = UIViewContentMode.center
+        iconDuration.image = #imageLiteral(resourceName: "assetMapDuration").withRenderingMode(UIImageRenderingMode.alwaysTemplate)
+        iconDuration.tintColor = colour
         
         let labelDistance:UILabel = UILabel()
         labelDistance.isUserInteractionEnabled = false
         labelDistance.backgroundColor = UIColor.clear
         labelDistance.translatesAutoresizingMaskIntoConstraints = false
-        labelDistance.textColor = UIColor.colourBackgroundDark
+        labelDistance.textColor = colour
+        labelDistance.font = UIFont.regular(size:VHomeReadyBarListHeader.Constants.fontSize)
         self.labelDistance = labelDistance
         
         let labelDuration:UILabel = UILabel()
         labelDuration.backgroundColor = UIColor.clear
         labelDuration.isUserInteractionEnabled = false
         labelDuration.translatesAutoresizingMaskIntoConstraints = false
-        labelDuration.textColor = UIColor.colourBackgroundDark
+        labelDuration.textColor = colour
+        labelDuration.font = UIFont.regular(size:VHomeReadyBarListHeader.Constants.fontSize)
         self.labelDuration = labelDuration
         
         addSubview(border)
         addSubview(labelDuration)
         addSubview(labelDistance)
+        addSubview(iconDistance)
+        addSubview(iconDuration)
         
         NSLayoutConstraint.bottomToBottom(
             view:border,
@@ -35,5 +57,43 @@ extension VHomeReadyBarListHeader
         NSLayoutConstraint.equalsHorizontal(
             view:border,
             toView:self)
+        
+        NSLayoutConstraint.equalsVertical(
+            view:iconDistance,
+            toView:self)
+        NSLayoutConstraint.leftToLeft(
+            view:iconDistance,
+            toView:self)
+        NSLayoutConstraint.width(
+            view:iconDistance,
+            constant:VHomeReadyBarListHeader.Constants.iconWidth)
+        
+        NSLayoutConstraint.equalsVertical(
+            view:iconDuration,
+            toView:self)
+        NSLayoutConstraint.rightToRight(
+            view:iconDuration,
+            toView:self)
+        NSLayoutConstraint.width(
+            view:iconDuration,
+            constant:VHomeReadyBarListHeader.Constants.iconWidth)
+        
+        NSLayoutConstraint.equalsVertical(
+            view:labelDistance,
+            toView:self)
+        NSLayoutConstraint.leftToRight(
+            view:labelDistance,
+            toView:iconDistance)
+        NSLayoutConstraint.widthGreaterOrEqual(
+            view:labelDistance)
+        
+        NSLayoutConstraint.equalsVertical(
+            view:labelDuration,
+            toView:self)
+        NSLayoutConstraint.rightToLeft(
+            view:labelDuration,
+            toView:iconDuration)
+        NSLayoutConstraint.widthGreaterOrEqual(
+            view:labelDuration)
     }
 }
