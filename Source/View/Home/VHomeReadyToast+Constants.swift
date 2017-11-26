c03f9250b217eb2fa2e4cfbfe096a4efeebceb0a
vauxhall <zero.griffin@gmail.com>
2017-11-25 21:39:45 -0600

    Create constants extension

---
+++ Source/View/Home/VHomeReadyToast+Constants.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+extension VHomeReadyToast
+{
+    struct Constants
+    {
+        static let paddingHorizontal:CGFloat = 10
+        static let paddingVertical:CGFloat = 20
+    }
+}
