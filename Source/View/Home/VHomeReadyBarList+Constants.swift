cdf6a5e0345beaab53f56f4d388235bdd6bf159e
vauxhall <zero.griffin@gmail.com>
2017-11-25 18:09:13 -0600

    Update header layout

--- Source/View/Home/VHomeReadyBarList+Constants.swift
+++ Source/View/Home/VHomeReadyBarList+Constants.swift
@@ -4,7 +4,7 @@ extension VHomeReadyBarList
 {
     struct Constants
     {
-        static let headerHeight:CGFloat = 60
+        static let headerHeight:CGFloat = 50
         static let cellHeight:CGFloat = 54
         static let collectionBottom:CGFloat = 10
     }
--- Source/View/Home/VHomeReadyBarListHeader+Factory.swift
+++ Source/View/Home/VHomeReadyBarListHeader+Factory.swift
@@ -8,7 +8,7 @@ extension VHomeReadyBarListHeader
     {
         let border:VBorder = VBorder(colour:UIColor.colourBackgroundGray)
       
-        let colour:UIColor = UIColor.colourBackgroundDark.withAlphaComponent(0.5)
+        let colour:UIColor = UIColor.colourBackgroundDark.withAlphaComponent(0.45)
         
         let iconDistance:UIImageView = UIImageView()
         iconDistance.translatesAutoresizingMaskIntoConstraints = false
