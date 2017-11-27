b666a662c8c55d21f2813e10645c02de0f0a385a
vauxhall <zero.griffin@gmail.com>
2017-11-26 18:00:18 -0600

    Animate toast

--- Source/View/Home/VHomeReadyToast+Constants.swift
+++ Source/View/Home/VHomeReadyToast+Constants.swift
@@ -5,5 +5,6 @@ extension VHomeReadyToast
     struct Constants
     {
         static let fontSize:CGFloat = 16
+        static let duration:TimeInterval = 4
     }
 }
--- Source/View/Home/VHomeReadyToast.swift
+++ Source/View/Home/VHomeReadyToast.swift
@@ -7,12 +7,14 @@ final class VHomeReadyToast:UIView
     init(message:String)
     {
         super.init(frame:CGRect.zero)
+        alpha = 0
         clipsToBounds = true
         translatesAutoresizingMaskIntoConstraints = false
         isUserInteractionEnabled = false
         backgroundColor = UIColor(white:0, alpha:0.9)
         
         factoryMessage(message:message)
+        animateShow()
     }
     
     required init?(coder:NSCoder)
@@ -23,6 +25,15 @@ final class VHomeReadyToast:UIView
     deinit
     {
         timer?.invalidate()
+    }
+    
+    //MARK: selectors
+    
+    @objc
+    private func selectorTimer(sender timer:Timer)
+    {
+        timer.invalidate()
+        animateHide()
     }
     
     //MARK: private
@@ -43,5 +54,43 @@ final class VHomeReadyToast:UIView
         NSLayoutConstraint.equals(
             view:label,
             toView:self)
+    }
+    
+    private func startTimer()
+    {
+        timer = Timer.scheduledTimer(
+            timeInterval:VHomeReadyToast.Constants.duration,
+            target:self,
+            selector:#selector(selectorTimer(sender:)),
+            userInfo:nil,
+            repeats:false)
+    }
+    
+    private func animateShow()
+    {
+        UIView.animate(withDuration:ViewMain.Constants.animationDuration,
+        animations:
+        { [weak self] in
+            
+            self?.alpha = 1
+        })
+        { [weak self] (done:Bool) in
+            
+            self?.startTimer()
+        }
+    }
+    
+    private func animateHide()
+    {
+        UIView.animate(withDuration:ViewMain.Constants.animationDuration,
+       animations:
+        { [weak self] in
+            
+            self?.alpha = 0
+        })
+        { [weak self] (done:Bool) in
+            
+            self?.removeFromSuperview()
+        }
     }
 }
