8bf502fc923f6051a6bd02f46da7ba475a1e850d
vauxhall <zero.griffin@gmail.com>
2017-11-27 16:28:37 -0600

    Create map menu protocol

---
+++ Source/Model/Home/MHomeMapMenuProtocol.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+protocol MHomeMapMenuProtocol
+{
+    var statusType:MHomeMapStatusProtocol.Type? { get }
+    var icon:UIImage { get }
+    var iconSelected:UIImage { get }
+    
+    func selected(controller:CCreate)
+}
