0155aaadbe070fd831c1d836b59f4cbc1d712def
vauxhall <zero.griffin@gmail.com>
2017-11-28 17:58:38 -0600

    Layout list cell

--- Source/View/PlansDetail/VPlansDetailListCell+Factory.swift
+++ Source/View/PlansDetail/VPlansDetailListCell+Factory.swift
@@ -6,6 +6,41 @@ extension VPlansDetailListCell
     
     func factoryViews()
     {
+        let icon:UIImageView = UIImageView()
+        icon.isUserInteractionEnabled = false
+        icon.translatesAutoresizingMaskIntoConstraints = false
+        icon.clipsToBounds = true
+        icon.contentMode = UIViewContentMode.center
+        icon.image = #imageLiteral(resourceName: "assetGenericStop").withRenderingMode(UIImageRenderingMode.alwaysTemplate)
+        icon.tintColor = UIColor.colourBackgroundDark
         
+        let labelTitle:UILabel = UILabel()
+        labelTitle.isUserInteractionEnabled = false
+        labelTitle.backgroundColor = UIColor.clear
+        labelTitle.translatesAutoresizingMaskIntoConstraints = false
+        labelTitle.font = UIFont.regular(size:VPlansDetailListCell.Constants.fontSize)
+        labelTitle.textColor = UIColor.colourBackgroundDark
+        self.labelTitle = labelTitle
+        
+        addSubview(labelTitle)
+        
+        NSLayoutConstraint.leftToRight(
+            view:labelTitle,
+            toView:icon)
+        NSLayoutConstraint.equalsVertical(
+            view:labelTitle,
+            toView:self)
+        NSLayoutConstraint.widthGreaterOrEqual(
+            view:labelTitle)
+        
+        NSLayoutConstraint.equalsVertical(
+            view:icon,
+            toView:self)
+        NSLayoutConstraint.leftToLeft(
+            view:icon,
+            toView:self)
+        NSLayoutConstraint.width(
+            view:icon,
+            constant:VPlansDetailListCell.Constants.iconWidth)
     }
 }
--- Source/View/PlansDetail/VPlansDetailListCell.swift
+++ Source/View/PlansDetail/VPlansDetailListCell.swift
@@ -2,10 +2,27 @@ import UIKit
 
 final class VPlansDetailListCell:UICollectionViewCell
 {
+    weak var labelTitle:UILabel!
+    
+    override init(frame:CGRect)
+    {
+        super.init(frame:frame)
+        clipsToBounds = true
+        backgroundColor = UIColor.clear
+        isUserInteractionEnabled = false
+        
+        factoryViews()
+    }
+    
+    required init?(coder:NSCoder)
+    {
+        return nil
+    }
+    
     //MARK: internal
     
     func config(model:DPlanStop)
     {
-        
+        labelTitle.text = model.name
     }
 }
