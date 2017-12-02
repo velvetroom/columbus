1a97f26fc9460a26766df5829195b5a29ce5749d
vauxhall <zero.griffin@gmail.com>
2017-12-02 00:02:29 -0600

    Update map pins

--- Source/View/Global/Map/VMapPin+Constants.swift
+++ Source/View/Global/Map/VMapPin+Constants.swift
@@ -7,8 +7,8 @@ extension VMapPin
         static let dragStateMap:[MKAnnotationViewDragState:MKAnnotationViewDragState] = [
             MKAnnotationViewDragState.starting : MKAnnotationViewDragState.dragging]
         
-        static let imageOffsetY:CGFloat = -17
-        static let imageDragginOffsetY:CGFloat = 24
+        static let imageOffsetY:CGFloat = 0
+        static let imageDragginOffsetY:CGFloat = 60
         static let animationDuration:TimeInterval = 0.3
     }
 }
