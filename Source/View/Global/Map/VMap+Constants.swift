549dbc8cf0523f859144ef14b5dfe1b5ef91aa8e
vauxhall <zero.griffin@gmail.com>
2017-12-01 23:32:13 -0600

    Refactor view map constants

---
+++ Source/View/Global/Map/VMap+Constants.swift
@@ -0,0 +1,13 @@
+import UIKit
+import CoreLocation
+
+extension VMap
+{
+    struct Constants
+    {
+        static let rendererLineDash:[NSNumber] = [1, 10]
+        static let rendererWidth:CGFloat = 6
+        static let distanceAccuracy:CLLocationDistance = 100
+        static let spanSize:CLLocationDegrees = 0.003
+    }
+}
