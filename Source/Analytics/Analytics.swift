8511e3b3091c1ba47ae1c9f82e945c4d7885f16d
vauxhall <zero.griffin@gmail.com>
2017-12-06 19:12:52 -0600

    Add manual screen tracking for analytics

--- Source/Analytics/Analytics.swift
+++ Source/Analytics/Analytics.swift
@@ -2,12 +2,19 @@ import Foundation
 import Firebase
 
 final class Analytics
-{
+{    
     //MARK: internal
     
     class func start()
     {
-        FirebaseConfiguration.shared.setLoggerLevel(FirebaseLoggerLevel.min)
+        FirebaseConfiguration.shared.setLoggerLevel(FirebaseLoggerLevel.max)
         FirebaseApp.configure()
+    }
+    
+    class func setScreenView(controller:UIViewController)
+    {
+        let controllerType:UIViewController.Type = type(of:controller)
+        let controllerName:String = String(describing:controllerType)
+        Firebase.Analytics.setScreenName(controllerName, screenClass:controllerName)
     }
 }
